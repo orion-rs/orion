da56fdc2da3d86510f2b4524ba40b9acf9ad19c5
brycx <brycx@protonmail.com>
2018-02-07 23:06:44 +0100

    Added most changes from Shepmaster Code Review

--- src/hmac.rs
+++ src/hmac.rs
@@ -1,98 +1,95 @@
 use functions;
-use ring::{digest, test};
+use ring::digest;
 
-// Set blocksizes
-pub const BLOCKSIZE_256: usize = 64;
-pub const BLOCKSIZE_512: usize = 128;
-
-/// Return either a SHA256 or SHA512 digest of byte vector
-fn hash(variant: i32, mut data: Vec<u8>) -> Vec<u8> {
-    if variant == 256 {
-        data = (digest::digest(&digest::SHA256, &data).as_ref()).to_vec();
-    } else if variant == 512 {
-        data = (digest::digest(&digest::SHA512, &data).as_ref()).to_vec();
-    } else {
-        panic!("Invalid variant. Valid variants are 256 and 512.");
-    }
-    return data;
+enum Variant {
+    SHA256,
+    SHA512,
 }
 
-/// Return a key k that has been padded to fit the selected blocksize
-fn key_deriv(variant: i32, mut k: Vec<u8>) -> Vec<u8> {
-    if variant == 256 {
-        // If key k is bigger than blocksize, it should be hashed and then padded with zeroes
-        // to fit blocksize
-        if k.len() > BLOCKSIZE_256 {
-            k = hash(variant, k);
+impl Variant {
+
+    fn blocksize(&self) -> usize {
+        match *self {
+            Variant::SHA256 => 64,
+            Variant::SHA512 => 128,
         }
-        while k.len() < BLOCKSIZE_256 {
-            k.push(0x00);
-        }
-        return k;
-    } else if variant == 512 {
-        if k.len() > BLOCKSIZE_512 {
-            k = hash(variant, k);
-        }
-        while k.len() < BLOCKSIZE_512 {
-            k.push(0x00);
-        }
-        return k;
-    } else {
-        panic!("Invalid variant. Valid variants are 256 and 512.");
     }
-}
+    /// Return either a SHA256 or SHA512 digest of byte vector
+    fn hash(&self, data: &[u8]) -> Vec<u8> {
+        let method = match *self {
+            Variant::SHA256 => &digest::SHA256,
+            Variant::SHA512 => &digest::SHA512,
+        };
+        digest::digest(method, data).as_ref().to_vec()
+    }
+    /// Return a padded key such that it fits the blocksize
+    fn pad_key(&self, key: &[u8]) -> Vec<u8> {
+        let mut resized_key = key.to_vec();
 
-/// Returns an HMAC from message m and key k
-pub fn hmac(variant: i32, mut k: Vec<u8>, mut m: Vec<u8>) -> Vec<u8> {
-    // Initialize vectors that will hold the ipad and opad
-    let mut ipad = vec![];
-    let mut opad = vec![];
-    // Pad the key
-    k = key_deriv(variant, k);
-
-    for count in 0..k.len() {
-        ipad.push(k[count] ^ 0x36);
-        opad.push(k[count] ^ 0x5C);
+        if resized_key.len() > self.blocksize() {
+            resized_key = self.hash(key);
+        }
+        if resized_key.len() < self.blocksize() {
+            resized_key.resize(self.blocksize(), 0x00);
+        }
+        resized_key
     }
 
-    ipad.append(&mut m);
-    ipad = hash(variant, ipad);
-    opad.append(&mut ipad);
-    opad = hash(variant, opad);
+    /// Returns an HMAC from key and message
+    pub fn hmac(&self, key: &[u8], message: &[u8]) -> Vec<u8> {
 
-    return opad;
+        let make_padded_key = |byte: u8| {
+            let mut pad = self.pad_key(key);
+            for i in &mut pad { *i ^= byte };
+            pad
+        };
+
+        let mut ipad = make_padded_key(0x36);
+        let mut opad = make_padded_key(0x5C);
+
+        ipad.extend_from_slice(message);
+        ipad = self.hash(&ipad);
+        opad.extend_from_slice(&ipad);
+        self.hash(&opad)
+    }
 }
 
+#[cfg(test)]
+mod test {
+    use hmac::Variant;
+    use ring::test;
+    use functions;
 
-#[test]
-// Test that the function key_deriv() returns a padded key K
-// with size of correct BLOCKSIZE, both for SHA256 and SHA512
-fn test_key_deriv() {
-    let rand_k: Vec<u8> = functions::gen_rand_key(64);
-    let rand_k2: Vec<u8> = functions::gen_rand_key(128);
-    let rand_k3: Vec<u8> = functions::gen_rand_key(34);
-    assert_eq!(key_deriv(256, rand_k.clone()).len(), BLOCKSIZE_256);
-    assert_eq!(key_deriv(512, rand_k.clone()).len(), BLOCKSIZE_512);
-    assert_eq!(key_deriv(256, rand_k2.clone()).len(), BLOCKSIZE_256);
-    assert_eq!(key_deriv(512, rand_k2.clone()).len(), BLOCKSIZE_512);
-    assert_eq!(key_deriv(256, rand_k3.clone()).len(), BLOCKSIZE_256);
-    assert_eq!(key_deriv(512, rand_k3.clone()).len(), BLOCKSIZE_512);
-}
+    #[test]
+    // Test that the function key_deriv() returns a padded key K
+    // with size of correct BLOCKSIZE, both for SHA256 and SHA512
+    fn test_key_deriv() {
+        let rand_k: Vec<u8> = functions::gen_rand_key(64);
+        let rand_k2: Vec<u8> = functions::gen_rand_key(128);
+        let rand_k3: Vec<u8> = functions::gen_rand_key(34);
+        assert_eq!(Variant::SHA256.pad_key(&rand_k).len(), Variant::SHA256.blocksize());
+        assert_eq!(Variant::SHA512.pad_key(&rand_k).len(), Variant::SHA512.blocksize());
+        assert_eq!(Variant::SHA256.pad_key(&rand_k2).len(), Variant::SHA256.blocksize());
+        assert_eq!(Variant::SHA512.pad_key(&rand_k2).len(), Variant::SHA512.blocksize());
+        assert_eq!(Variant::SHA256.pad_key(&rand_k3).len(), Variant::SHA256.blocksize());
+        assert_eq!(Variant::SHA512.pad_key(&rand_k3).len(), Variant::SHA512.blocksize());
+    }
 
-#[test]
-// Test that hmac() returns expected HMAC digests
-fn test_hmac_digest_result() {
-    let k_256 = vec![0x61; BLOCKSIZE_256];
-    let m_256 = vec![0x62; BLOCKSIZE_256];
-    let actual_256 = hmac(256, k_256, m_256);
+    #[test]
+    // Test that hmac() returns expected HMAC digests
+    fn test_hmac_digest_result() {
+        let k_256 = vec![0x61; Variant::SHA256.blocksize()];
+        let m_256 = vec![0x62; Variant::SHA256.blocksize()];
+        let actual_256 = Variant::SHA256.hmac(&k_256, &m_256);
 
-    let k_512 = vec![0x63; BLOCKSIZE_256];
-    let m_512 = vec![0x64; BLOCKSIZE_256];
-    let actual_512 = hmac(512, k_512, m_512);
+        let k_512 = vec![0x63; Variant::SHA256.blocksize()];
+        let m_512 = vec![0x64; Variant::SHA256.blocksize()];
+        let actual_512 = Variant::SHA512.hmac(&k_512, &m_512);
 
-    // Expected values from: https://www.freeformatter.com/hmac-generator.html#ad-output
-    let expected_256 = test::from_hex("f6cbb37b326d36f2f27d294ac3bb46a6aac29c1c9936b985576041bfb338ae70").unwrap();
-    let expected_512 = test::from_hex("ffbd423817836ae58b801fc1e70386f09a6cc0e72daa215ac8505993721f0f6d67ce30118d7effe451310abad984d105fbd847ae37a88f042a3a79e26f307606").unwrap();
-    assert_eq!(actual_256, expected_256);
-    assert_eq!(actual_512, expected_512);
+        // Expected values from: https://www.freeformatter.com/hmac-generator.html#ad-output
+        let expected_256 = test::from_hex("f6cbb37b326d36f2f27d294ac3bb46a6aac29c1c9936b985576041bfb338ae70").unwrap();
+        let expected_512 = test::from_hex("ffbd423817836ae58b801fc1e70386f09a6cc0e72daa215ac8505993721f0f6d67ce30118d7effe451310abad984d105fbd847ae37a88f042a3a79e26f307606").unwrap();
+        assert_eq!(actual_256, expected_256);
+        assert_eq!(actual_512, expected_512);
+    }
 }
