eec636111fe74ec288ab53b3af820373cfe5f513
brycx <brycx@protonmail.com>
2018-04-25 15:13:51 +0200

    Added test vectors for SHA384 and SHA512

--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -152,7 +152,7 @@ mod test {
     // The script that has been used to generate the expected test vectors can be found
     // in the pbkdf2-test-vectors folder.
     #[test]
-    fn gen_test_case_1() {
+    fn sha256_test_case_1() {
 
         let pbkdf2_dk_256 = Pbkdf2 {
             password: "password".as_bytes().to_vec(),
@@ -170,7 +170,7 @@ mod test {
     }
 
     #[test]
-    fn gen_test_case_2() {
+    fn sha256_test_case_2() {
 
         let pbkdf2_dk_256 = Pbkdf2 {
             password: "password".as_bytes().to_vec(),
@@ -188,7 +188,7 @@ mod test {
     }
 
     #[test]
-    fn gen_test_case_3() {
+    fn sha256_test_case_3() {
 
         let pbkdf2_dk_256 = Pbkdf2 {
             password: "password".as_bytes().to_vec(),
@@ -208,7 +208,7 @@ mod test {
     // Commented out because it takes about 10 minues to complete
     /*
     #[test]
-    fn gen_test_case_4() {
+    fn sha256_test_case_4() {
 
         let pbkdf2_dk_256 = Pbkdf2 {
             password: "password".as_bytes().to_vec(),
@@ -228,7 +228,7 @@ mod test {
     */
 
     #[test]
-    fn gen_test_case_5() {
+    fn sha256_test_case_5() {
 
         let pbkdf2_dk_256 = Pbkdf2 {
             password: "passwordPASSWORDpassword".as_bytes().to_vec(),
@@ -246,7 +246,7 @@ mod test {
     }
 
     #[test]
-    fn gen_test_case_6() {
+    fn sha256_test_case_6() {
 
         let pbkdf2_dk_256 = Pbkdf2 {
             password: "pass\0word".as_bytes().to_vec(),
@@ -261,6 +261,230 @@ mod test {
         ).unwrap();
 
         assert_eq!(expected_pbkdf2_dk_256, pbkdf2_dk_256.pbkdf2_compute());
+    }
+
+    #[test]
+    fn sha384_test_case_1() {
+
+        let pbkdf2_dk_384 = Pbkdf2 {
+            password: "password".as_bytes().to_vec(),
+            salt: "salt".as_bytes().to_vec(),
+            iterations: 1,
+            length: 20,
+            hmac: ShaVariantOption::SHA384,
+        };
+
+        let expected_pbkdf2_dk_384 = decode(
+            "c0e14f06e49e32d73f9f52ddf1d0c5c719160923"
+        ).unwrap();
+
+        assert_eq!(expected_pbkdf2_dk_384, pbkdf2_dk_384.pbkdf2_compute());
+    }
+
+    #[test]
+    fn sha384_test_case_2() {
+
+        let pbkdf2_dk_384 = Pbkdf2 {
+            password: "password".as_bytes().to_vec(),
+            salt: "salt".as_bytes().to_vec(),
+            iterations: 2,
+            length: 20,
+            hmac: ShaVariantOption::SHA384,
+        };
+
+        let expected_pbkdf2_dk_384 = decode(
+            "54f775c6d790f21930459162fc535dbf04a93918"
+        ).unwrap();
+
+        assert_eq!(expected_pbkdf2_dk_384, pbkdf2_dk_384.pbkdf2_compute());
+    }
+
+    #[test]
+    fn sha384_test_case_3() {
+
+        let pbkdf2_dk_384 = Pbkdf2 {
+            password: "password".as_bytes().to_vec(),
+            salt: "salt".as_bytes().to_vec(),
+            iterations: 4096,
+            length: 20,
+            hmac: ShaVariantOption::SHA384,
+        };
+
+        let expected_pbkdf2_dk_384 = decode(
+            "559726be38db125bc85ed7895f6e3cf574c7a01c"
+        ).unwrap();
+
+        assert_eq!(expected_pbkdf2_dk_384, pbkdf2_dk_384.pbkdf2_compute());
+    }
+
+    // Commented out because it takes about 10 minues to complete
+    /*
+    #[test]
+    fn sha384_test_case_4() {
+
+        let pbkdf2_dk_384 = Pbkdf2 {
+            password: "password".as_bytes().to_vec(),
+            salt: "salt".as_bytes().to_vec(),
+            iterations: 16777216,
+            length: 20,
+            hmac: ShaVariantOption::SHA384,
+        };
+
+        let expected_pbkdf2_dk_384 = decode(
+            "a7fdb349ba2bfa6bf647bb0161bae1320df27e64"
+        ).unwrap();
+
+        assert_eq!(expected_pbkdf2_dk_384, pbkdf2_dk_384.pbkdf2_compute());
+
+    }
+    */
+
+    #[test]
+    fn sha384_test_case_5() {
+
+        let pbkdf2_dk_384 = Pbkdf2 {
+            password: "passwordPASSWORDpassword".as_bytes().to_vec(),
+            salt: "saltSALTsaltSALTsaltSALTsaltSALTsalt".as_bytes().to_vec(),
+            iterations: 4096,
+            length: 25,
+            hmac: ShaVariantOption::SHA384,
+        };
+
+        let expected_pbkdf2_dk_384 = decode(
+            "819143ad66df9a552559b9e131c52ae6c5c1b0eed18f4d283b"
+        ).unwrap();
+
+        assert_eq!(expected_pbkdf2_dk_384, pbkdf2_dk_384.pbkdf2_compute());
+    }
+
+    #[test]
+    fn sha384_test_case_6() {
+
+        let pbkdf2_dk_384 = Pbkdf2 {
+            password: "pass\0word".as_bytes().to_vec(),
+            salt: "sa\0lt".as_bytes().to_vec(),
+            iterations: 4096,
+            length: 16,
+            hmac: ShaVariantOption::SHA384,
+        };
+
+        let expected_pbkdf2_dk_384 = decode(
+            "a3f00ac8657e095f8e0823d232fc60b3"
+        ).unwrap();
+
+        assert_eq!(expected_pbkdf2_dk_384, pbkdf2_dk_384.pbkdf2_compute());
+    }
+
+    #[test]
+    fn sha512_test_case_1() {
+
+        let pbkdf2_dk_512 = Pbkdf2 {
+            password: "password".as_bytes().to_vec(),
+            salt: "salt".as_bytes().to_vec(),
+            iterations: 1,
+            length: 20,
+            hmac: ShaVariantOption::SHA512,
+        };
+
+        let expected_pbkdf2_dk_512 = decode(
+            "867f70cf1ade02cff3752599a3a53dc4af34c7a6"
+        ).unwrap();
+
+        assert_eq!(expected_pbkdf2_dk_512, pbkdf2_dk_512.pbkdf2_compute());
+    }
+
+    #[test]
+    fn sha512_test_case_2() {
+
+        let pbkdf2_dk_512 = Pbkdf2 {
+            password: "password".as_bytes().to_vec(),
+            salt: "salt".as_bytes().to_vec(),
+            iterations: 2,
+            length: 20,
+            hmac: ShaVariantOption::SHA512,
+        };
+
+        let expected_pbkdf2_dk_512 = decode(
+            "e1d9c16aa681708a45f5c7c4e215ceb66e011a2e"
+        ).unwrap();
+
+        assert_eq!(expected_pbkdf2_dk_512, pbkdf2_dk_512.pbkdf2_compute());
+    }
+
+    #[test]
+    fn sha512_test_case_3() {
+
+        let pbkdf2_dk_512 = Pbkdf2 {
+            password: "password".as_bytes().to_vec(),
+            salt: "salt".as_bytes().to_vec(),
+            iterations: 4096,
+            length: 20,
+            hmac: ShaVariantOption::SHA512,
+        };
+
+        let expected_pbkdf2_dk_512 = decode(
+            "d197b1b33db0143e018b12f3d1d1479e6cdebdcc"
+        ).unwrap();
+
+        assert_eq!(expected_pbkdf2_dk_512, pbkdf2_dk_512.pbkdf2_compute());
+    }
+
+    // Commented out because it takes about 10 minues to complete
+    /*
+    #[test]
+    fn sha512_test_case_4() {
+
+        let pbkdf2_dk_512 = Pbkdf2 {
+            password: "password".as_bytes().to_vec(),
+            salt: "salt".as_bytes().to_vec(),
+            iterations: 16777216,
+            length: 20,
+            hmac: ShaVariantOption::SHA512,
+        };
+
+        let expected_pbkdf2_dk_512 = decode(
+            "6180a3ceabab45cc3964112c811e0131bca93a35"
+        ).unwrap();
+
+        assert_eq!(expected_pbkdf2_dk_512, pbkdf2_dk_512.pbkdf2_compute());
+
+    }
+    */
+
+    #[test]
+    fn sha512_test_case_5() {
+
+        let pbkdf2_dk_512 = Pbkdf2 {
+            password: "passwordPASSWORDpassword".as_bytes().to_vec(),
+            salt: "saltSALTsaltSALTsaltSALTsaltSALTsalt".as_bytes().to_vec(),
+            iterations: 4096,
+            length: 25,
+            hmac: ShaVariantOption::SHA512,
+        };
+
+        let expected_pbkdf2_dk_512 = decode(
+            "8c0511f4c6e597c6ac6315d8f0362e225f3c501495ba23b868"
+        ).unwrap();
+
+        assert_eq!(expected_pbkdf2_dk_512, pbkdf2_dk_512.pbkdf2_compute());
+    }
+
+    #[test]
+    fn sha512_test_case_6() {
+
+        let pbkdf2_dk_512 = Pbkdf2 {
+            password: "pass\0word".as_bytes().to_vec(),
+            salt: "sa\0lt".as_bytes().to_vec(),
+            iterations: 4096,
+            length: 16,
+            hmac: ShaVariantOption::SHA512,
+        };
+
+        let expected_pbkdf2_dk_512 = decode(
+            "9d9e9c4cd21fe4be24d5b8244c759665"
+        ).unwrap();
+
+        assert_eq!(expected_pbkdf2_dk_512, pbkdf2_dk_512.pbkdf2_compute());
     }
 
     #[test]
