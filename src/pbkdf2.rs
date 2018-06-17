86310e76172494ea830eccc94ca85a5932d18314
brycx <brycx@protonmail.com>
2018-06-17 18:15:15 +0200

    Doc: Further improvements

--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -57,7 +57,8 @@ impl Drop for Pbkdf2 {
 /// ## Note:
 /// Salts should always be generated using a CSPRNG. The `gen_rand_key` function
 /// in `util` can be used for this. The recommended length for a salt is the output length of the
-/// hash function. So if using SHA512 then a salt of 64 bytes is recommended.
+/// hash function. So if using SHA512 then a salt of 64 bytes is recommended. The iteration count
+/// should be set as high as feasible.
 /// # Usage examples:
 /// ### Generating derived key:
 /// ```
