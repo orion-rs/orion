5af77002ba66ccb9e937452d542a833490971afc
brycx <brycx@protonmail.com>
2018-06-20 20:53:31 +0200

    Fuzz: Use rand range for hkdf

--- fuzz/fuzz_targets/hkdf.rs
+++ fuzz/fuzz_targets/hkdf.rs
@@ -5,27 +5,28 @@ extern crate rand;
 
 use orion::hazardous::hkdf::Hkdf;
 use orion::core::options::ShaVariantOption;
+use rand::prelude::*;
 
-fn fuzz_hkdf(salt: &[u8], ikm: &[u8], info: &[u8], okm_len: usize, hmac: ShaVariantOption) {
 
-    // Make sure to cover all possible length selections
-    for okm_len_inter in 1..okm_len+1 {
+fn fuzz_hkdf(salt: &[u8], ikm: &[u8], info: &[u8], len_max: usize, hmac: ShaVariantOption) {
 
-        let dk = Hkdf {
-            salt: salt.to_vec(),
-            ikm: ikm.to_vec(),
-            info: info.to_vec(),
-            length: okm_len_inter,
-            hmac,
-        };
+    let mut rng = rand::thread_rng();
 
-        let prk = dk.extract(ikm, salt);
-        let dk_fin = dk.expand(&prk).unwrap();
+    let okm_len_rand = rng.gen_range(1, len_max+1);
 
-        assert_eq!(dk_fin, dk.derive_key().unwrap());
-        assert_eq!(dk.verify(&dk_fin).unwrap(), true);
+    let dk = Hkdf {
+        salt: salt.to_vec(),
+        ikm: ikm.to_vec(),
+        info: info.to_vec(),
+        length: okm_len_rand,
+        hmac,
+    };
 
-    }
+    let prk = dk.extract(ikm, salt);
+    let dk_fin = dk.expand(&prk).unwrap();
+
+    assert_eq!(dk_fin, dk.derive_key().unwrap());
+    assert_eq!(dk.verify(&dk_fin).unwrap(), true);
 }
 
 fuzz_target!(|data: &[u8]| {
