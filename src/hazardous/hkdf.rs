6fa68957211f65a082a5cff63ab595acbe085f0c
brycx <brycx@protonmail.com>
2018-08-12 19:31:43 +0200

    Fix: #19

--- src/hazardous/hkdf.rs
+++ src/hazardous/hkdf.rs
@@ -20,30 +20,14 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-use clear_on_drop::clear::Clear;
-use core::options::ShaVariantOption;
-use core::{errors::*, util};
-use hazardous::hmac::Hmac;
+use hazardous::constants::{HLenArray, HLEN};
+use hazardous::hmac;
+use utilities::{errors::*, util};
 
 /// HKDF (HMAC-based Extract-and-Expand Key Derivation Function) as specified in the
 /// [RFC 5869](https://tools.ietf.org/html/rfc5869).
 ///
 /// Fields `salt`, `ikm` and `info` are zeroed out on drop.
-pub struct Hkdf {
-    pub salt: Vec<u8>,
-    pub ikm: Vec<u8>,
-    pub info: Vec<u8>,
-    pub length: usize,
-    pub hmac: ShaVariantOption,
-}
-
-impl Drop for Hkdf {
-    fn drop(&mut self) {
-        Clear::clear(&mut self.salt);
-        Clear::clear(&mut self.ikm);
-        Clear::clear(&mut self.info)
-    }
-}
 
 /// HKDF (HMAC-based Extract-and-Expand Key Derivation Function) as specified in the
 /// [RFC 5869](https://tools.ietf.org/html/rfc5869).
@@ -51,8 +35,7 @@ impl Drop for Hkdf {
 /// - `salt`:  Optional salt value
 /// - `ikm`: Input keying material
 /// - `info`: Optional context and application specific information (can be a zero-length string)
-/// - `length`: Length of output keying material
-/// - `hmac`: HMAC function
+/// - `okm_out`: Destination buffer for the derivec key. The length of the derived key is implied by the length of `okm_out`
 ///
 /// See [RFC](https://tools.ietf.org/html/rfc5869#section-2.2) for more information.
 ///
@@ -70,123 +53,78 @@ impl Drop for Hkdf {
 /// # Example:
 /// ### Generating derived key:
 /// ```
-/// use orion::hazardous::hkdf::Hkdf;
-/// use orion::core::util::gen_rand_key;
-/// use orion::core::options::ShaVariantOption;
-///
-/// let key = gen_rand_key(32).unwrap();
-/// let salt = gen_rand_key(32).unwrap();
-/// let info = gen_rand_key(32).unwrap();
-///
-/// let dk = Hkdf {
-///     salt: salt,
-///     ikm: key,
-///     info: info,
-///     length: 50,
-///     hmac: ShaVariantOption::SHA256,
-/// };
-///
-/// let dk_final = dk.derive_key().unwrap();
+/// use orion::hazardous::hkdf;
 /// ```
 /// ### Verifying derived key:
 /// ```
-/// use orion::hazardous::hkdf::Hkdf;
-/// use orion::core::util::gen_rand_key;
-/// use orion::core::options::ShaVariantOption;
-///
-/// let key = gen_rand_key(32).unwrap();
-/// let salt = gen_rand_key(32).unwrap();
-/// let info = gen_rand_key(32).unwrap();
-///
-/// let dk = Hkdf {
-///     salt: salt,
-///     ikm: key,
-///     info: info,
-///     length: 50,
-///     hmac: ShaVariantOption::SHA256,
-/// };
-///
-/// let dk_final = dk.derive_key().unwrap();
-///
-/// assert_eq!(dk.verify(&dk_final).unwrap(), true);
+/// use orion::hazardous::hkdf;
 /// ```
 
-impl Hkdf {
-    /// Return the maximum okm length (255 * hLen).
-    fn max_okmlen(&self) -> usize {
-        match self.hmac.output_size() {
-            32 => 8160,
-            48 => 12240,
-            64 => 16320,
-            _ => panic!(UnknownCryptoError),
-        }
+#[inline(always)]
+pub fn extract(salt: &[u8], ikm: &[u8]) -> HLenArray {
+    let mut prk = hmac::init(salt);
+    prk.update(ikm);
+
+    prk.finalize()
+}
+
+#[inline(always)]
+pub fn expand(prk: &[u8], info: &[u8], okm_out: &mut [u8]) -> Result<(), UnknownCryptoError> {
+    if okm_out.len() > 16320 {
+        return Err(UnknownCryptoError);
+    }
+    if okm_out.len() < 1 {
+        return Err(UnknownCryptoError);
     }
 
-    /// The HKDF Extract step.
-    pub fn extract(&self, salt: &[u8], ikm: &[u8]) -> Vec<u8> {
-        let prk = Hmac {
-            secret_key: salt.to_vec(),
-            data: ikm.to_vec(),
-            sha2: self.hmac,
-        };
+    let mut hmac = hmac::init(prk);
 
-        prk.finalize()
-    }
+    for (idx, hlen_block) in okm_out.chunks_mut(HLEN).enumerate() {
+        hmac.update(info);
+        hmac.update(&[idx as u8 + 1_u8]);
 
-    /// The HKDF Expand step.
-    pub fn expand(&self, prk: &[u8]) -> Result<Vec<u8>, UnknownCryptoError> {
-        if self.length > self.max_okmlen() {
-            return Err(UnknownCryptoError);
-        }
-        if self.length < 1 {
-            return Err(UnknownCryptoError);
-        }
+        let block_len = hlen_block.len();
+        //hlen_block.copy_from_slice(&hmac.finalize_with_opad(&opad, )[..block_len]);
+        hmac.finalize_with_dst(&mut hlen_block[..block_len]);
 
-        let n_iter: usize = 1 + ((self.length - 1) / self.hmac.output_size());
-
-        // con_step will hold the intermediate state of "T_n | info | 0x0n" as described in the RFC
-        let mut con_step: Vec<u8> = Vec::new();
-        let mut okm: Vec<u8> = Vec::new();
-
-        for index in 1..n_iter + 1 {
-            con_step.extend_from_slice(&self.info);
-            con_step.push(index as u8);
-            // Given that n_iter is rounded correctly, then the `index as u8`
-            // should not be able to overflow. If the maximum okmlen is selected,
-            // along with the highest output size, then n_iter will equal exactly `u8::max_value()`
-
-            // Calling extract as it yields the same result as an HMAC call
-            con_step = self.extract(prk, &con_step);
-            okm.extend_from_slice(&con_step);
-        }
-
-        okm.truncate(self.length);
-
-        Ok(okm)
-    }
-
-    /// Combine Extract and Expand to return a derived key.
-    pub fn derive_key(&self) -> Result<Vec<u8>, UnknownCryptoError> {
-        let mut prk = self.extract(&self.salt, &self.ikm);
-
-        let dk = self.expand(&prk);
-
-        Clear::clear(&mut prk);
-
-        dk
-    }
-
-    /// Verify a derived key by comparing one from the current struct fields to the derived key
-    /// passed to the function. Comparison is done in constant time. Both derived keys must be
-    /// of equal length.
-    pub fn verify(&self, expected_dk: &[u8]) -> Result<bool, ValidationCryptoError> {
-        let own_dk = self.derive_key().unwrap();
-
-        if util::compare_ct(&own_dk, expected_dk).is_err() {
-            Err(ValidationCryptoError)
+        // Check if it's the last iteration, if yes don't process anything
+        if block_len < HLEN {
+            break;
         } else {
-            Ok(true)
+            hmac.reset();
+            hmac.update(&hlen_block);
         }
+    }
+
+    Ok(())
+}
+
+/// Combine Extract and Expand to return a derived key.
+pub fn derive_key(
+    salt: &[u8],
+    ikm: &[u8],
+    info: &[u8],
+    okm_out: &mut [u8],
+) -> Result<(), UnknownCryptoError> {
+    expand(&extract(salt, ikm), info, okm_out)
+}
+
+/// Verify a derived key by comparing one from the current struct fields to the derived key
+/// passed to the function. Comparison is done in constant time. Both derived keys must be
+/// of equal length.
+pub fn verify(
+    expected_dk: &[u8],
+    salt: &[u8],
+    ikm: &[u8],
+    info: &[u8],
+    okm_out: &mut [u8],
+) -> Result<bool, ValidationCryptoError> {
+    expand(&extract(salt, ikm), info, okm_out).unwrap();
+
+    if util::compare_ct(&okm_out, expected_dk).is_err() {
+        Err(ValidationCryptoError)
+    } else {
+        Ok(true)
     }
 }
 
@@ -194,147 +132,91 @@ impl Hkdf {
 mod test {
     extern crate hex;
     use self::hex::decode;
-    use core::options::ShaVariantOption;
-    use hazardous::hkdf::Hkdf;
-
-    #[test]
-    fn hkdf_maximum_length_256() {
-        let hkdf = Hkdf {
-            salt: decode("").unwrap(),
-            ikm: decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap(),
-            info: decode("").unwrap(),
-            // Max allowed length here is 8160
-            length: 9000,
-            hmac: ShaVariantOption::SHA256,
-        };
-
-        let hkdf_extract = hkdf.extract(&hkdf.ikm, &hkdf.salt);
-
-        assert!(hkdf.expand(&hkdf_extract).is_err());
-        assert!(hkdf.derive_key().is_err());
-    }
-
-    #[test]
-    fn hkdf_maximum_length_384() {
-        let hkdf = Hkdf {
-            salt: decode("").unwrap(),
-            ikm: decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap(),
-            info: decode("").unwrap(),
-            // Max allowed length here is 12240
-            length: 13000,
-            hmac: ShaVariantOption::SHA384,
-        };
-
-        let hkdf_extract = hkdf.extract(&hkdf.ikm, &hkdf.salt);
-
-        assert!(hkdf.expand(&hkdf_extract).is_err());
-        assert!(hkdf.derive_key().is_err());
-    }
+    use hazardous::hkdf::*;
 
     #[test]
     fn hkdf_maximum_length_512() {
-        let hkdf = Hkdf {
-            salt: decode("").unwrap(),
-            ikm: decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap(),
-            info: decode("").unwrap(),
-            // Max allowed length here is 16320
-            length: 17000,
-            hmac: ShaVariantOption::SHA512,
-        };
+        // Max allowed length here is 16320
+        let mut okm_out = [0u8; 17000];
+        let prk = extract("".as_bytes(), "".as_bytes());
 
-        let hkdf_extract = hkdf.extract(&hkdf.ikm, &hkdf.salt);
-
-        assert!(hkdf.expand(&hkdf_extract).is_err());
-        assert!(hkdf.derive_key().is_err());
+        assert!(expand(&prk, "".as_bytes(), &mut okm_out).is_err());
     }
 
     #[test]
     fn hkdf_zero_length() {
-        let hkdf = Hkdf {
-            salt: decode("").unwrap(),
-            ikm: decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap(),
-            info: decode("").unwrap(),
-            length: 0,
-            hmac: ShaVariantOption::SHA512,
-        };
+        let mut okm_out = [0u8; 0];
+        let prk = extract("".as_bytes(), "".as_bytes());
 
-        let hkdf_extract = hkdf.extract(&hkdf.ikm, &hkdf.salt);
-
-        assert!(hkdf.expand(&hkdf_extract).is_err());
-        assert!(hkdf.derive_key().is_err());
+        assert!(expand(&prk, "".as_bytes(), &mut okm_out).is_err());
     }
 
     #[test]
     fn hkdf_verify_true() {
-        let hkdf = Hkdf {
-            salt: decode("").unwrap(),
-            ikm: decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap(),
-            info: decode("").unwrap(),
-            length: 42,
-            hmac: ShaVariantOption::SHA256,
-        };
+        let ikm = decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap();
+        let salt = decode("000102030405060708090a0b0c").unwrap();
+        let info = decode("f0f1f2f3f4f5f6f7f8f9").unwrap();
+        let mut okm_out = [0u8; 42];
 
         let expected_okm = decode(
-            "8da4e775a563c18f715f802a063c5a31b8a11f5c5ee1879ec3454e5f3c738d2d\
-             9d201395faa4b61a96c8",
+            "832390086cda71fb47625bb5ceb168e4c8e26a1a16ed34d9fc7fe92c1481579338da362cb8d9f925d7cb",
         ).unwrap();
 
-        assert_eq!(hkdf.verify(&expected_okm).unwrap(), true);
+
+        assert_eq!(
+            verify(&expected_okm, &salt, &ikm, &info, &mut okm_out).unwrap(),
+            true
+        );
     }
 
     #[test]
     fn hkdf_verify_wrong_salt() {
-        // Salt value differs between this and the previous test case
-        let hkdf = Hkdf {
-            salt: "salt".as_bytes().to_vec(),
-            ikm: decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap(),
-            info: decode("").unwrap(),
-            length: 42,
-            hmac: ShaVariantOption::SHA256,
-        };
+        let salt = "salt".as_bytes();
+        let ikm = decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap();
+        let info = "".as_bytes();
+        let mut okm_out = [0u8; 42];
 
         let expected_okm = decode(
             "8da4e775a563c18f715f802a063c5a31b8a11f5c5ee1879ec3454e5f3c738d2d\
              9d201395faa4b61a96c8",
         ).unwrap();
 
-        assert!(hkdf.verify(&expected_okm).is_err());
+        assert!(
+            verify(&expected_okm, salt, &ikm, info, &mut okm_out).is_err()
+        );
     }
 
     #[test]
     fn hkdf_verify_wrong_ikm() {
-        let hkdf = Hkdf {
-            salt: decode("").unwrap(),
-            ikm: decode("0b").unwrap(),
-            info: decode("").unwrap(),
-            length: 42,
-            hmac: ShaVariantOption::SHA256,
-        };
+        let salt = "".as_bytes();
+        let ikm = decode("0b").unwrap();
+        let info = "".as_bytes();
+        let mut okm_out = [0u8; 42];
 
         let expected_okm = decode(
             "8da4e775a563c18f715f802a063c5a31b8a11f5c5ee1879ec3454e5f3c738d2d\
              9d201395faa4b61a96c8",
         ).unwrap();
 
-        assert!(hkdf.verify(&expected_okm).is_err());
+        assert!(
+            verify(&expected_okm, salt, &ikm, info, &mut okm_out).is_err()
+        );
     }
 
     #[test]
-    fn verify_diff_length_panic() {
-        // Different length than expected okm
-        let hkdf = Hkdf {
-            salt: "salt".as_bytes().to_vec(),
-            ikm: decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap(),
-            info: decode("").unwrap(),
-            length: 75,
-            hmac: ShaVariantOption::SHA256,
-        };
+    fn verify_diff_length() {
+        let salt = "".as_bytes();
+        let ikm = decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap();
+        let info = "".as_bytes();
+        let mut okm_out = [0u8; 72];
 
         let expected_okm = decode(
             "8da4e775a563c18f715f802a063c5a31b8a11f5c5ee1879ec3454e5f3c738d2d\
              9d201395faa4b61a96c8",
         ).unwrap();
 
-        assert!(hkdf.verify(&expected_okm).is_err());
+        assert!(
+            verify(&expected_okm, salt, &ikm, info, &mut okm_out).is_err()
+        );
     }
 }
