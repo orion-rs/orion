eeb0faf059971f5811c8a905374b9d5ee431e457
brycx <brycx@protonmail.com>
2018-08-13 16:51:28 +0200

    Update benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,68 +2,43 @@
 extern crate orion;
 extern crate test;
 
-use orion::core::options::KeccakVariantOption;
-use orion::core::options::ShaVariantOption;
-use orion::hazardous::cshake::CShake;
-use orion::hazardous::hkdf::Hkdf;
-use orion::hazardous::hmac::Hmac;
-use orion::hazardous::pbkdf2::Pbkdf2;
+use orion::hazardous::cshake;
+use orion::hazardous::hkdf;
+use orion::hazardous::hmac;
+use orion::hazardous::pbkdf2;
 use test::Bencher;
 
 #[bench]
 fn bench_hmac(b: &mut Bencher) {
     b.iter(|| {
-        let hmac = Hmac {
-            secret_key: vec![0x01; 32],
-            data: vec![0x01; 32],
-            sha2: ShaVariantOption::SHA256,
-        };
-
-        hmac.finalize();
+        let mut mac = hmac::init(&vec![0x01; 64]);
+        mac.update(&vec![0x01; 64]);
+        mac.finalize();
     });
 }
 
 #[bench]
 fn bench_hkdf(b: &mut Bencher) {
     b.iter(|| {
-        let hkdf = Hkdf {
-            salt: vec![0x01; 32],
-            ikm: vec![0x01; 32],
-            info: vec![0x01; 32],
-            length: 32,
-            hmac: ShaVariantOption::SHA256,
-        };
-
-        hkdf.derive_key().unwrap();
+        let mut okm_out = [0u8; 64];
+        hkdf::derive_key(&vec![0x01; 64], &vec![0x01; 64], &vec![0x01; 64], &mut okm_out).unwrap();
     });
 }
 
 #[bench]
 fn bench_pbkdf2(b: &mut Bencher) {
     b.iter(|| {
-        let pbkdf = Pbkdf2 {
-            salt: vec![0x01; 32],
-            password: vec![0x01; 32],
-            iterations: 10000,
-            dklen: 32,
-            hmac: ShaVariantOption::SHA256,
-        };
-
-        pbkdf.derive_key().unwrap();
+        let mut dk_out = [0u8; 64];
+        pbkdf2::derive_key(&vec![0x01; 64], &vec![0x01; 64], 10000, &mut dk_out).unwrap();
     });
 }
 
 #[bench]
 fn bench_cshake(b: &mut Bencher) {
     b.iter(|| {
-        let cshake = CShake {
-            input: vec![0x01; 32],
-            name: vec![0x00; 0],
-            custom: vec![0x01; 32],
-            length: 64,
-            keccak: KeccakVariantOption::KECCAK256,
-        };
-
-        cshake.finalize().unwrap();
+        let mut hash_out = [0u8; 64];
+        let mut cshake = cshake::init(&vec![0x01; 64], None).unwrap();
+        cshake.update(&vec![0x01; 64]);
+        cshake.finalize(&mut hash_out).unwrap();
     });
 }
