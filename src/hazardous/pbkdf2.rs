1a1fe992500cde5a22fee3122af1cf0f8749a7ac
brycx <brycx@protonmail.com>
2018-08-13 18:10:37 +0200

    Typo

--- src/hazardous/pbkdf2.rs
+++ src/hazardous/pbkdf2.rs
@@ -124,7 +124,7 @@ pub fn derive_key(
 
     for (idx, dk_block) in dk_out.chunks_mut(HLEN).enumerate() {
         function_f(salt, iterations, idx + 1, dk_block, &mut hmac);
-        // Reset the HMAC state to clear the data from tis iteration
+        // Reset the HMAC state to clear the data from this iteration
         hmac.reset();
     }
 
