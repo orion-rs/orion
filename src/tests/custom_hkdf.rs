bba94fb22f00c67ba44ec5baa4c9ec27cfe4c0aa
brycx <brycx@protonmail.com>
2018-08-14 16:24:48 +0200

    Fix hex encoding

--- src/tests/custom_hkdf.rs
+++ src/tests/custom_hkdf.rs
@@ -47,13 +47,13 @@ mod custom_hkdf {
 
     #[test]
     fn test_case_1() {
-        let ikm = "0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b".as_bytes();
-        let salt = "000102030405060708090a0b0c".as_bytes();
+        let ikm = decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap();
+        let salt = decode("000102030405060708090a0b0c").unwrap();
         let info = decode("").unwrap();
         let mut okm = [0u8; 32];
 
         let expected_okm =
-            decode("55f4e219c32e613b33c637e4795379e886c8babb53cfbb9b8fa201fc7fdf604a").unwrap();
+            decode("f81b87481a18b664936daeb222f58cba0ebc55f5c85996b9f1cb396c327b70bb").unwrap();
 
         assert!(hkdf_test_runner(
             &expected_okm,
@@ -67,19 +67,18 @@ mod custom_hkdf {
     #[test]
     fn test_case_2() {
         let ikm =
-            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f2\
+            decode("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f2\
              02122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f40414243444546474849\
-             4a4b4c4d4e4f"
-                .as_bytes();
+             4a4b4c4d4e4f").unwrap();
         let salt = "salt".as_bytes();
         let info = "random InF\0".as_bytes();
         let mut okm = [0u8; 128];
 
         let expected_okm = decode(
-            "56b03ea00ca8ef5bf639027ae10af7c0392f619a63b40cf61f305505f4e2c039df6e1baae41\
-             f0f8545bba840afe297d8da514b03892233076ad0a38a5215d21c37757bb753c60cc03b2b71b\
-             b4cad3fced96a9038b4b4369c9863879dd2e17d0c84d889e07f57a4502e2137b042a0e913974f\
-             adbee53ff94a5bdae6aaae333766",
+            "a246ef99f6a0f783fc004682508e6f288f036469788f004fcbac9414caa889fa175e746ee663914d\
+            678c155d510fa536f7d49b1054e85e7751d9745ea02079a78608eec9aacdd82fa9421d6223c158c71\
+            b76bcf9008b50e8aac027a73f98643eb3947106b65c0bc9a2983404fd4d0fce0735d639379b193470\
+            9c8b2999b5989e",
         ).unwrap();
 
         assert!(hkdf_test_runner(
@@ -95,21 +94,19 @@ mod custom_hkdf {
     fn test_case_3() {
         let ikm = "password".as_bytes();
         let salt =
-            "606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7\
+            decode("606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7\
              f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a\
-             8a9aaabacadaeaf"
-                .as_bytes();
+             8a9aaabacadaeaf").unwrap();
         let info = decode("").unwrap();
         let mut okm = [0u8; 256];
 
-        let expected_okm = decode(
-            "b59f1a1f19a28aa1401bb5c3b8a6fded08d39ee15ae4101687585116fca3bd63ab64b9ba83f9\
-             675cfe97e7012a9b7fbce263c2b9c952ab07891d7cf4110846424f74a74c15a37d2124ee5962e\
-             94f5a00af64cdfdf59afc58af18dc43652860b4502770a8fd6997ed130546624ed26b75c8c7799\
-             54e8ba286a324e9cdad8babaf609ef12c84f3d7351a391bce365839a20ebf59e23dc22854d73c\
-             8386536c36261f67410309178097e40d46fe62bfe24d7e71c1a1bf854e62f8300d24c72137067b\
-             d63e401606c5ece5fa31fba62bd3fc05d1906672c5b12605bd12ab8b2c3e216437d32973890d5\
-             1f9d713cc11ddbd40ef32fda2d581c4ac3f9ba8bdc823bdbe",
+        let expected_okm = decode("245d63179146a61ca1a25f92c38391d406bb52da4b773714fb0e43ce90\
+        84ce430f43e1980a8817cf0af320fb684776d81f674d2b187449d62200d3e39cb51ab7a444f7964944895\
+        ad36b37432fb400fdca0181a9ebda41f9d124d58f8a696dde9bd104a93fbbe3c93b94dd06a2254894b489\
+        822ab08daa791f8962a492a6a7379e8710b46fe85c8bf9d64a957641164577d5b5afdaf8fad1fb3879a3c\
+        8bc8425b9f265462b59785e7cf7855e6c571353c38907a8d9b0a01c228bb3a1792039e8728ea01c939160\
+        1f1626da771f65f2322116ddc4e192d98da81b0402fd664ef89801a4905d9557be5c7f01bf8381fae7d32\
+        5c3dc7a5795dc760b9668eb63f8ee",
         ).unwrap();
 
         assert!(hkdf_test_runner(
@@ -123,19 +120,18 @@ mod custom_hkdf {
 
     #[test]
     fn test_case_4() {
-        let ikm = "0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b".as_bytes();
+        let ikm = decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap();
         let salt = "s$#$#%$#SBGHWE@#W#lt".as_bytes();
         let info = "random InF\0".as_bytes();
         let mut okm = [0u8; 256];
 
-        let expected_okm = decode(
-            "2033ff164f1c52fff45984131a8e791da83f41b681d33f254cbd9fdc2e376f485ef07a5132a\
-             461a43efa454439b75dc270177a9871b4ceccc1c4d354d89a8e22ca4362839e55576088f3a3d\
-             b677b3c7c2841f1f49bd646519020a0ac868390d52d3c96497c125bcf5a7083ad10422ec0b8f\
-             9a0c3f9d8eaa049173a67282d8393fdd8f24d78bb458b02eba60137c445f456c6e1922b8db02\
-             706efee900f5aa4b49cf407f8ac85297a09c29e2a16ef3a02da90ce8268a150cfa1e5325926f\
-             0bdefae6dabea86af6401d67c3c9a62e21db7ade26641628301d321f6d000034eae6e570321b\
-             bb8eb2a36bd954b7c39dcb950b1bf55e1b3deffc06855f77513950d03",
+        let expected_okm = decode("e93182a8af74a1e70a6202075759bbbceb1926a18aa9f9ee31796557\
+        0b507cea7ef11f94d83760bb6f8a2f6031edb581c1ae43f45ead820223d34c6ffadab43d3cfaf9cd782\
+        b8aa7bd2ebab8663b51d4e40b9a659a7e262630581fee55ac986770e88f580c8d8b82deba4d1c28bce4\
+        dc7a579456ed30a94a1782cab84699a4302ef8d24f23e9122ef2daaba4fd3d84c812c4b3a8d4788397f\
+        d38ddccf59d60a8330000cb04e5aa2d3e16e56dbccd8ca68020abcb3bc097788d38dfd2e241ba7772ba\
+        188c29d7f4d010b421875c9e7165ed2ebcf338b81071eca62300c9ca9840b6f1fc9403752536b3eca14\
+        7e9fbf127ff88d33b984582ced74fa029b50f441e",
         ).unwrap();
         assert!(hkdf_test_runner(
             &expected_okm,
@@ -169,13 +165,12 @@ mod custom_hkdf {
     fn test_case_6() {
         let ikm = "pass\0word".as_bytes();
         let salt = "saltSALTSALTSALTSALTSALTSALTSALTSALTSALTSALTSALTSALT".as_bytes();
-        let info = "b0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbccc\
+        let info = decode("b0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbccc\
                     dcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f\
-                    3f4f5f6f7f8f9fafbfcfdfeff"
-            .as_bytes();
+                    3f4f5f6f7f8f9fafbfcfdfeff").unwrap();
         let mut okm = [0u8; 16];
 
-        let expected_okm = decode("ae4107effdae85d156832ec28e6e84e6").unwrap();
+        let expected_okm = decode("8ae15623215eaaa156bad552f411c4ad").unwrap();
 
         assert!(hkdf_test_runner(
             &expected_okm,
