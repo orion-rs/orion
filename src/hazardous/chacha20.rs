127ea6670929244aad4adcd9e95979c5e8571567
brycx <brycx@protonmail.com>
2018-10-30 08:51:16 +0100

    Doc: Multiple accepted nonce lengths

--- src/hazardous/chacha20.rs
+++ src/hazardous/chacha20.rs
@@ -33,7 +33,7 @@
 //! # Exceptions:
 //! An exception will be thrown if:
 //! - The length of the `key` is not `32` bytes
-//! - The length of the `nonce` is not `12` bytes
+//! - The length of the `nonce` is not of an accepted length (check `constants` to see list)
 //! - The length of `dst_out` is less than `plaintext` or `ciphertext`
 //! - `plaintext` or `ciphertext` are empty
 //! - `plaintext` or `ciphertext` are longer than (2^32)-2
