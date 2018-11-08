49fcf50e2f27a568ff5d67fc2339a597d971ada7
brycx <brycx@protonmail.com>
2018-11-08 12:30:14 +0100

    Improve readability of constants and types

--- src/hazardous/constants.rs
+++ src/hazardous/constants.rs
@@ -24,12 +24,6 @@
 pub const SHA2_BLOCKSIZE: usize = 128;
 /// The output size for the hash function SHA512.
 pub const HLEN: usize = 64;
-/// Type for an array of length `SHA2_BLOCKSIZE`.
-pub type BlocksizeArray = [u8; SHA2_BLOCKSIZE];
-/// Type for an array of length `HLEN`.
-pub type HLenArray = [u8; HLEN];
-/// Type for a ChaCha state represented as an array of 16 32-bit unsigned integers.
-pub type ChaChaState = [u32; 16];
 /// The blocksize which ChaCha20 operates on.
 pub const CHACHA_BLOCKSIZE: usize = 64;
 /// The key size for ChaCha20.
@@ -44,7 +38,14 @@ pub const HCHACHA_NONCESIZE: usize = 16;
 pub const XCHACHA_NONCESIZE: usize = 24;
 /// The blocksize which Poly1305 operates on.
 pub const POLY1305_BLOCKSIZE: usize = 16;
-/// Type for a Poly1305 tag.
-pub type Poly1305Tag = [u8; 16];
 /// The key size for Poly1305.
 pub const POLY1305_KEYSIZE: usize = 32;
+
+/// Type for an array of length `SHA2_BLOCKSIZE`.
+pub type BlocksizeArray = [u8; SHA2_BLOCKSIZE];
+/// Type for an array of length `HLEN`.
+pub type HLenArray = [u8; HLEN];
+/// Type for a ChaCha state represented as an array of 16 32-bit unsigned integers.
+pub type ChaChaState = [u32; 16];
+/// Type for a Poly1305 tag.
+pub type Poly1305Tag = [u8; 16];
