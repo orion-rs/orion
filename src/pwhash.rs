c55d61ee254539784b4f12df53fbe7067eb2c0df
brycx <brycx@protonmail.com>
2018-11-16 13:17:37 +0100

    Docs: Rework of pwhash docs

--- src/pwhash.rs
+++ src/pwhash.rs
@@ -20,7 +20,39 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-//! Password hashing.
+//! Password hashing and verification.
+//!
+//! # About:
+//! - Uses PBKDF2-HMAC-SHA512
+//! - A salt of 64 bytes is automatically generated
+//! - The password hash length is set to 64
+//! - 512.000 iterations are used
+//! - An array of 128 bytes is returned
+//!
+//! The first 64 bytes of the array returned by `pwhash::hash_password` is the salt used to hash the password
+//! and the last 64 bytes is the actual hashed password. When using this function with
+//! `pwhash::verify_password_hash()`, then the seperation of the salt and the password hash are automatically handeled.
+//!
+//! # Parameters:
+//! - `password`: The password to be hashed
+//! - `expected_hash`: The expected password hash
+//!
+//! # Exceptions:
+//! An exception will be thrown if:
+//! - The `OsRng` fails to initialize or read from its source
+//! - The `expected_hash` is not 128 bytes
+//! - The `expected_hash` is not constructed exactly as in `pwhash::hash_password`
+//! - The password hash does not match `expected_hash`
+//!
+//! # Example:
+//! ```
+//! use orion::pwhash;
+//!
+//! let password = pwhash::Password::from_slice("Secret password".as_bytes());
+//!
+//! let hash = pwhash::hash_password(&password).unwrap();
+//! assert!(pwhash::verify_password_hash(&derived_password, &password).unwrap());
+//! ```
 
 use errors::{UnknownCryptoError, ValidationCryptoError};
 use hazardous::kdf::pbkdf2;
@@ -29,35 +61,7 @@ use util;
 
 #[must_use]
 /// Hash a password using PBKDF2-HMAC-SHA512.
-/// # About:
-/// This is meant to be used for password storage.
-/// - A salt of 64 bytes is automatically generated.
-/// - The derived key length is set to 64.
-/// - 512.000 iterations are used.
-/// - An array of 128 bytes is returned.
-///
-/// The first 64 bytes of this array is the salt used to derive the key and the last 64 bytes
-/// is the actual derived key. When using this function with `default::password_hash_verify()`,
-/// then the seperation of the salt and the derived key are automatically handeled.
-///
-/// # Parameters:
-/// - `password` : The password to be hashed
-///
-/// # Exceptions:
-/// An exception will be thrown if:
-/// - The length of the password is less than 14 bytes
-/// - The `OsRng` fails to initialize or read from its source
-///
-/// # Example:
-///
-/// ```
-/// use orion::default::pwhash;
-///
-/// let password = pwhash::Password::from_slice("Secret password".as_bytes());
-///
-/// let derived_password = pwhash::password_hash(&password);
-/// ```
-pub fn password_hash(password: &Password) -> Result<[u8; 128], UnknownCryptoError> {
+pub fn hash_password(password: &Password) -> Result<[u8; 128], UnknownCryptoError> {
     let mut dk = [0u8; 128];
     let mut salt = [0u8; 64];
     util::secure_rand_bytes(&mut salt).unwrap();
@@ -70,44 +74,20 @@ pub fn password_hash(password: &Password) -> Result<[u8; 128], UnknownCryptoErro
 
 #[must_use]
 /// Verify a hashed password using PBKDF2-HMAC-SHA512.
-/// # About:
-/// This function is meant to be used with the `default::password_hash()` function in orion's default API. It can be
-/// used without it, but then the `expected_dk` passed to the function must be constructed just as in
-/// `default::password_hash()`. See documention on `default::password_hash()` for details on this.
-///
-/// # Parameters:
-/// - `expected_dk`: The expected password hash
-/// - `password` : The password to be hashed
-///
-/// # Exceptions:
-/// An exception will be thrown if:
-/// - The length of `expected_dk` is not 128 bytes
-/// - The password hash does not match the expected
-///
-/// # Example:
-///
-/// ```
-/// use orion::default::pwhash;
-///
-/// let password = pwhash::Password::from_slice("Secret password".as_bytes());
-///
-/// let derived_password = pwhash::password_hash(&password).unwrap();
-/// assert!(pwhash::password_hash_verify(&derived_password, &password).unwrap());
-/// ```
-pub fn password_hash_verify(
-    expected_dk: &[u8],
+pub fn verify_password_hash(
+    expected_hash: &[u8],
     password: &Password,
 ) -> Result<bool, ValidationCryptoError> {
-    if expected_dk.len() != 128 {
+    if expected_hash.len() != 128 {
         return Err(ValidationCryptoError);
     }
 
     let mut dk = [0u8; 64];
 
     pbkdf2::verify(
-        &expected_dk[64..],
+        &expected_hash[64..],
         password,
-        &expected_dk[..64],
+        &expected_hash[..64],
         512_000,
         &mut dk,
     )
@@ -117,9 +97,9 @@ pub fn password_hash_verify(
 fn pbkdf2_verify() {
     let password = Password::from_slice(&[0u8; 64]);
 
-    let pbkdf2_dk: [u8; 128] = password_hash(&password).unwrap();
+    let pbkdf2_dk: [u8; 128] = hash_password(&password).unwrap();
 
-    assert_eq!(password_hash_verify(&pbkdf2_dk, &password).unwrap(), true);
+    assert_eq!(verify_password_hash(&pbkdf2_dk, &password).unwrap(), true);
 }
 
 #[test]
@@ -127,10 +107,10 @@ fn pbkdf2_verify() {
 fn pbkdf2_verify_err_modified_salt() {
     let password = Password::from_slice(&[0u8; 64]);
 
-    let mut pbkdf2_dk = password_hash(&password).unwrap();
+    let mut pbkdf2_dk = hash_password(&password).unwrap();
     pbkdf2_dk[..10].copy_from_slice(&[0x61; 10]);
 
-    password_hash_verify(&pbkdf2_dk, &password).unwrap();
+    verify_password_hash(&pbkdf2_dk, &password).unwrap();
 }
 
 #[test]
@@ -138,10 +118,10 @@ fn pbkdf2_verify_err_modified_salt() {
 fn pbkdf2_verify_err_modified_password() {
     let password = Password::from_slice(&[0u8; 64]);
 
-    let mut pbkdf2_dk = password_hash(&password).unwrap();
+    let mut pbkdf2_dk = hash_password(&password).unwrap();
     pbkdf2_dk[70..80].copy_from_slice(&[0x61; 10]);
 
-    password_hash_verify(&pbkdf2_dk, &password).unwrap();
+    verify_password_hash(&pbkdf2_dk, &password).unwrap();
 }
 
 #[test]
@@ -149,10 +129,10 @@ fn pbkdf2_verify_err_modified_password() {
 fn pbkdf2_verify_err_modified_salt_and_password() {
     let password = Password::from_slice(&[0u8; 64]);
 
-    let mut pbkdf2_dk = password_hash(&password).unwrap();
+    let mut pbkdf2_dk = hash_password(&password).unwrap();
     pbkdf2_dk[63..73].copy_from_slice(&[0x61; 10]);
 
-    password_hash_verify(&pbkdf2_dk, &password).unwrap();
+    verify_password_hash(&pbkdf2_dk, &password).unwrap();
 }
 
 #[test]
@@ -160,16 +140,16 @@ fn pbkdf2_verify_expected_dk_too_long() {
     let password = Password::from_slice(&[0u8; 64]);
 
     let mut pbkdf2_dk = [0u8; 129];
-    pbkdf2_dk[..128].copy_from_slice(&password_hash(&password).unwrap());
+    pbkdf2_dk[..128].copy_from_slice(&hash_password(&password).unwrap());
 
-    assert!(password_hash_verify(&pbkdf2_dk, &password).is_err());
+    assert!(verify_password_hash(&pbkdf2_dk, &password).is_err());
 }
 
 #[test]
 fn pbkdf2_verify_expected_dk_too_short() {
     let password = Password::from_slice(&[0u8; 127]);
 
-    let pbkdf2_dk = password_hash(&password).unwrap();
+    let pbkdf2_dk = hash_password(&password).unwrap();
 
-    assert!(password_hash_verify(&pbkdf2_dk[..127], &password).is_err());
+    assert!(verify_password_hash(&pbkdf2_dk[..127], &password).is_err());
 }
