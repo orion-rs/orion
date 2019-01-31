795b2d2fcf7ac9d4abab523fd1f4c5724bb81caf
brycx <brycx@protonmail.com>
2019-01-31 18:50:46 +0100

    Refactor to impl_load and impl_store and add proptests

--- src/util/endianness.rs
+++ src/util/endianness.rs
@@ -22,172 +22,47 @@
 
 use core::mem;
 
-macro_rules! impl_store_le {
+macro_rules! impl_store {
     ($(#[$meta:meta])*
-    ($type_alias: ty, $func_name: ident)) => (
+    ($type_alias: ty, $conv_function: ident, $func_name: ident)) => (
         #[inline]
         $(#[$meta])*
 		///
         pub fn $func_name(src: &[$type_alias], dst: &mut [u8]) {
             let type_alias_len = mem::size_of::<$type_alias>();
             assert!((type_alias_len * src.len()) == dst.len());
-            
+
             for (src_elem, dst_chunk) in src.iter().zip(dst.chunks_exact_mut(type_alias_len)) {
-                dst_chunk.copy_from_slice(&src_elem.to_le_bytes());
+                dst_chunk.copy_from_slice(&src_elem.$conv_function());
             }
         }
 	);
 }
 
-macro_rules! impl_store_be {
+macro_rules! impl_load {
     ($(#[$meta:meta])*
-    ($type_alias: ty, $func_name: ident)) => (
-        #[inline]
-        $(#[$meta])*
-		///
-        pub fn $func_name(src: &[$type_alias], dst: &mut [u8]) {
-            let type_alias_len = mem::size_of::<$type_alias>();
-            assert!((type_alias_len * src.len()) == dst.len());
-            
-            for (src_elem, dst_chunk) in src.iter().zip(dst.chunks_exact_mut(type_alias_len)) {
-                dst_chunk.copy_from_slice(&src_elem.to_be_bytes());
-            }
-        }
-	);
-}
-
-macro_rules! impl_load_le {
-    ($(#[$meta:meta])*
-    ($type_alias: ty, $type_alias_expr: ident, $func_name: ident)) => (
+    ($type_alias: ty, $type_alias_expr: ident, $conv_function: ident, $func_name: ident)) => (
         #[inline]
         $(#[$meta])*
 		///
         pub fn $func_name(src: &[u8], dst: &mut [$type_alias]) {
             let type_alias_len = mem::size_of::<$type_alias>();
-            assert!((type_alias_len * dst.len()) == src.len());
-            
+            assert!((dst.len() * type_alias_len) == src.len());
+
             let mut tmp = [0u8; mem::size_of::<$type_alias>()];
-            
+
             for (src_chunk, dst_elem) in src.chunks_exact(type_alias_len).zip(dst.iter_mut()) {
                 tmp.copy_from_slice(src_chunk);
-                *dst_elem = $type_alias_expr::from_le_bytes(tmp);
+                *dst_elem = $type_alias_expr::$conv_function(tmp);
             }
         }
     );
 }
 
-macro_rules! impl_load_be {
-    ($(#[$meta:meta])*
-    ($type_alias: ty, $type_alias_expr: ident, $func_name: ident)) => (
-        #[inline]
-        $(#[$meta])*
-		///
-        pub fn $func_name(src: &[u8], dst: &mut [$type_alias]) {
-            let type_alias_len = mem::size_of::<$type_alias>();
-            assert!((type_alias_len * dst.len()) == src.len());
-            
-            let mut tmp = [0u8; mem::size_of::<$type_alias>()];
-            
-            for (src_chunk, dst_elem) in src.chunks_exact(type_alias_len).zip(dst.iter_mut()) {
-                tmp.copy_from_slice(src_chunk);
-                *dst_elem = $type_alias_expr::from_be_bytes(tmp);
-            }
-        }
-
-    );
-}
-
-impl_store_le!(
-	/// Store bytes `src` in `dst` in little-endian byte order.
-	/// 
-	/// # Parameters:
-	/// - `dst`: Destination buffer.
-	/// - `src`: Source buffer.
-	///
-	/// # Exceptions:
-	/// An exception will be thrown if:
-	/// - `dst.len() != src.len() * 4`
-	/// 
-	/// # Example:
-	/// ```
-	/// use orion::util::endianness;
-	///
-	/// let mut dst = [0u8; 64];
-	///
-	/// endianness::store_u32_into_le(&[5u32; 16], &mut dst);
-	/// ```
-	(u32, store_u32_into_le)
-);
-
-impl_store_le!(
-	/// Store bytes in `src` into `dst` in little-endian byte order.
-	/// 
-	/// # Parameters:
-	/// - `dst`: Destination buffer.
-	/// - `src`: Source buffer.
-	///
-	/// # Exceptions:
-	/// An exception will be thrown if:
-	/// - `dst.len() != src.len() * 8`
-	/// 
-	/// # Example:
-	/// ```
-	/// use orion::util::endianness;
-	///
-	/// let mut dst = [0u8; 64];
-	///
-	/// endianness::store_u64_into_le(&[5u64; 8], &mut dst);
-	/// ```
-	(u64, store_u64_into_le)
-);
-
-impl_store_be!(
-	/// Store bytes in `src` into `dst` in big-endian byte order.
-	/// 
-	/// # Parameters:
-	/// - `dst`: Destination buffer.
-	/// - `src`: Source buffer.
-	///
-	/// # Exceptions:
-	/// An exception will be thrown if:
-	/// - `dst.len() != src.len() * 4`
-	/// 
-	/// # Example:
-	/// ```
-	/// use orion::util::endianness;
-	///
-	/// let mut dst = [0u8; 64];
-	///
-	/// endianness::store_u32_into_be(&[5u32; 16], &mut dst);
-	/// ```
-	(u32, store_u32_into_be)
-);
-
-impl_store_be!(
-	/// Store bytes in `src` into `dst` in big-endian byte order.
-	/// 
-	/// # Parameters:
-	/// - `dst`: Destination buffer.
-	/// - `src`: Source buffer.
-	///
-	/// # Exceptions:
-	/// An exception will be thrown if:
-	/// - `dst.len() != src.len() * 8`
-	/// 
-	/// # Example:
-	/// ```
-	/// use orion::util::endianness;
-	///
-	/// let mut dst = [0u8; 64];
-	///
-	/// endianness::store_u64_into_be(&[5u64; 8], &mut dst);
-	/// ```
-	(u64, store_u64_into_be)
-);
-
-impl_load_le!(
+#[rustfmt::skip]
+impl_load!(
 	/// Load bytes in `src` into `dst` in little-endian byte order.
-	/// 
+	///
 	/// # Parameters:
 	/// - `dst`: Destination buffer.
 	/// - `src`: Source buffer.
@@ -195,7 +70,7 @@ impl_load_le!(
 	/// # Exceptions:
 	/// An exception will be thrown if:
 	/// - `dst.len() != src.len() / 4`
-	/// 
+	///
 	/// # Example:
 	/// ```
 	/// use orion::util::endianness;
@@ -204,12 +79,13 @@ impl_load_le!(
 	///
 	/// endianness::load_u32_into_le(&[125u8; 64], &mut dst);
 	/// ```
-	(u32, u32, load_u32_into_le)
+	(u32, u32, from_le_bytes, load_u32_into_le)
 );
 
-impl_load_le!(
+#[rustfmt::skip]
+impl_load!(
 	/// Load bytes in `src` into `dst` in little-endian byte order.
-	/// 
+	///
 	/// # Parameters:
 	/// - `dst`: Destination buffer.
 	/// - `src`: Source buffer.
@@ -217,7 +93,7 @@ impl_load_le!(
 	/// # Exceptions:
 	/// An exception will be thrown if:
 	/// - `dst.len() != src.len() / 8`
-	/// 
+	///
 	/// # Example:
 	/// ```
 	/// use orion::util::endianness;
@@ -226,49 +102,276 @@ impl_load_le!(
 	///
 	/// endianness::load_u64_into_le(&[125u8; 64], &mut dst);
 	/// ```
-	(u64, u64, load_u64_into_le)
+	(u64, u64, from_le_bytes, load_u64_into_le)
 );
 
-impl_load_be!(
-	/// Load bytes in `src` into `dst` in big-endian byte order.
-	/// 
+#[rustfmt::skip]
+impl_load!(
+    /// Load bytes in `src` into `dst` in big-endian byte order.
+    ///
+    /// # Parameters:
+    /// - `dst`: Destination buffer.
+    /// - `src`: Source buffer.
+    ///
+    /// # Exceptions:
+    /// An exception will be thrown if:
+    /// - `dst.len() != src.len() / 4`
+    ///
+    /// # Example:
+    /// ```
+    /// use orion::util::endianness;
+    ///
+    /// let mut dst = [0u32; 16];
+    ///
+    /// endianness::load_u32_into_be(&[125u8; 64], &mut dst);
+    /// ```
+    (u32, u32, from_be_bytes, load_u32_into_be)
+);
+
+#[rustfmt::skip]
+impl_load!(
+    /// Load bytes in `src` into `dst` in big-endian byte order.
+    ///
+    /// # Parameters:
+    /// - `dst`: Destination buffer.
+    /// - `src`: Source buffer. 
+    ///
+    /// # Exceptions:
+    /// An exception will be thrown if:
+    /// - `dst.len() != src.len() / 8`
+    ///
+    /// # Example:
+    /// ```
+    /// use orion::util::endianness;
+    ///
+    /// let mut dst = [0u64; 8];
+    ///
+    /// endianness::load_u64_into_be(&[125u8; 64], &mut dst);
+    /// ```
+    (u64, u64, from_be_bytes, load_u64_into_be)
+);
+
+#[rustfmt::skip]
+impl_store!(
+    /// Store bytes `src` in `dst` in little-endian byte order.
+    ///
+    /// # Parameters:
+    /// - `dst`: Destination buffer.
+    /// - `src`: Source buffer.
+    ///
+    /// # Exceptions:
+    /// An exception will be thrown if:
+    /// - `dst.len() != src.len() * 4`
+    ///
+    /// # Example:
+    /// ```
+    /// use orion::util::endianness;
+    ///
+    /// let mut dst = [0u8; 64];
+    ///
+    /// endianness::store_u32_into_le(&[5u32; 16], &mut dst);
+    /// ```
+    (u32, to_le_bytes, store_u32_into_le)
+);
+
+#[rustfmt::skip]
+impl_store!(
+	/// Store bytes in `src` into `dst` in little-endian byte order.
+	///
 	/// # Parameters:
 	/// - `dst`: Destination buffer.
 	/// - `src`: Source buffer.
 	///
 	/// # Exceptions:
 	/// An exception will be thrown if:
-	/// - `dst.len() != src.len() / 4`
-	/// 
+	/// - `dst.len() != src.len() * 8`
+	///
 	/// # Example:
 	/// ```
 	/// use orion::util::endianness;
 	///
-	/// let mut dst = [0u32; 16];
+	/// let mut dst = [0u8; 64];
 	///
-	/// endianness::load_u32_into_be(&[125u8; 64], &mut dst);
+	/// endianness::store_u64_into_le(&[5u64; 8], &mut dst);
 	/// ```
-	(u32, u32, load_u32_into_be)
+	(u64, to_le_bytes, store_u64_into_le)
 );
 
-impl_load_be!(
-	/// Load bytes in `src` into `dst` in big-endian byte order.
-	/// 
+#[rustfmt::skip]
+impl_store!(
+	/// Store bytes in `src` into `dst` in big-endian byte order.
+	///
 	/// # Parameters:
 	/// - `dst`: Destination buffer.
 	/// - `src`: Source buffer.
 	///
 	/// # Exceptions:
 	/// An exception will be thrown if:
-	/// - `dst.len() != src.len() / 8`
-	/// 
+	/// - `dst.len() != src.len() * 4`
+	///
 	/// # Example:
 	/// ```
 	/// use orion::util::endianness;
 	///
-	/// let mut dst = [0u64; 8];
+	/// let mut dst = [0u8; 64];
 	///
-	/// endianness::load_u64_into_be(&[125u8; 64], &mut dst);
+	/// endianness::store_u32_into_be(&[5u32; 16], &mut dst);
 	/// ```
-	(u64, u64, load_u64_into_be)
+	(u32, to_be_bytes, store_u32_into_be)
 );
+
+#[rustfmt::skip]
+impl_store!(
+	/// Store bytes in `src` into `dst` in big-endian byte order.
+	///
+	/// # Parameters:
+	/// - `dst`: Destination buffer.
+	/// - `src`: Source buffer.
+	///
+	/// # Exceptions:
+	/// An exception will be thrown if:
+	/// - `dst.len() != src.len() * 8`
+	///
+	/// # Example:
+	/// ```
+	/// use orion::util::endianness;
+	///
+	/// let mut dst = [0u8; 64];
+	///
+	/// endianness::store_u64_into_be(&[5u64; 8], &mut dst);
+	/// ```
+	(u64, to_be_bytes, store_u64_into_be)
+);
+
+// Testing public functions in the module.
+#[cfg(test)]
+mod public {
+	use super::*;
+
+	// Proptests. Only exectued when NOT testing no_std.
+	#[cfg(feature = "safe_api")]
+	mod proptest {
+		use super::*;
+
+		quickcheck! {
+			/// Load and store should not change the result.
+			fn prop_load_store_u32_le(src: Vec<u8>) -> bool {
+				if !src.is_empty() && src.len() % 4 == 0 {
+					let mut dst_load = vec![0u32; src.len() / 4];
+                    assert!(dst_load.len() * 4 == src.len());
+					load_u32_into_le(&src[..], &mut dst_load);
+					let mut dst_store = src.clone();
+					store_u32_into_le(&dst_load[..], &mut dst_store);
+
+					(dst_store == src)
+				} else {
+					// if not, it panics
+					true
+				}
+			}
+		}
+
+		quickcheck! {
+			/// Load and store should not change the result.
+			fn prop_load_store_u32_be(src: Vec<u8>) -> bool {
+				if !src.is_empty() && src.len() % 4 == 0 {
+					let mut dst_load = vec![0u32; src.len() / 4];
+					load_u32_into_be(&src[..], &mut dst_load);
+					let mut dst_store = src.clone();
+					store_u32_into_be(&dst_load[..], &mut dst_store);
+
+					(dst_store == src)
+				} else {
+					// if not, it panics
+					true
+				}
+			}
+		}
+
+		quickcheck! {
+			/// Load and store should not change the result.
+			fn prop_load_store_u64_le(src: Vec<u8>) -> bool {
+				if !src.is_empty() && src.len() % 8 == 0 {
+					let mut dst_load = vec![0u64; src.len() / 8];
+					load_u64_into_le(&src[..], &mut dst_load);
+					let mut dst_store = src.clone();
+					store_u64_into_le(&dst_load[..], &mut dst_store);
+
+					(dst_store == src)
+				} else {
+					// if not, it panics
+					true
+				}
+			}
+		}
+
+		quickcheck! {
+			/// Load and store should not change the result.
+			fn prop_load_store_u64_be(src: Vec<u8>) -> bool {
+				if !src.is_empty() && src.len() % 8 == 0 {
+					let mut dst_load = vec![0u64; src.len() / 8];
+					load_u64_into_be(&src[..], &mut dst_load);
+					let mut dst_store = src.clone();
+					store_u64_into_be(&dst_load[..], &mut dst_store);
+
+					(dst_store == src)
+				} else {
+					// if not, it panics
+					true
+				}
+			}
+		}
+
+		quickcheck! {
+			/// Store and load should not change the result.
+			fn prop_store_load_u32_le(src: Vec<u32>) -> bool {
+
+				let mut dst_store = vec![0u8; src.len() * 4];
+				store_u32_into_le(&src[..], &mut dst_store);
+				let mut dst_load = src.clone();
+				load_u32_into_le(&dst_store[..], &mut dst_load);
+
+				(dst_load == src)
+			}
+		}
+
+		quickcheck! {
+			 /// Store and load should not change the result.
+			fn prop_store_load_u32_be(src: Vec<u32>) -> bool {
+
+				let mut dst_store = vec![0u8; src.len() * 4];
+				store_u32_into_be(&src[..], &mut dst_store);
+				let mut dst_load = src.clone();
+				load_u32_into_be(&dst_store[..], &mut dst_load);
+
+				(dst_load == src)
+			}
+		}
+
+		quickcheck! {
+			 /// Store and load should not change the result.
+			fn prop_store_load_u64_le(src: Vec<u64>) -> bool {
+
+				let mut dst_store = vec![0u8; src.len() * 8];
+				store_u64_into_le(&src[..], &mut dst_store);
+				let mut dst_load = src.clone();
+				load_u64_into_le(&dst_store[..], &mut dst_load);
+
+				(dst_load == src)
+			}
+		}
+
+		quickcheck! {
+			 /// Store and load should not change the result.
+			fn prop_store_load_u64_be(src: Vec<u64>) -> bool {
+
+				let mut dst_store = vec![0u8; src.len() * 8];
+				store_u64_into_be(&src[..], &mut dst_store);
+				let mut dst_load = src.clone();
+				load_u64_into_be(&dst_store[..], &mut dst_load);
+
+				(dst_load == src)
+			}
+		}
+	}
+}
