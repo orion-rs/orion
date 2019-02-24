ba6609d750278016bcd82d5b9d903134ea2ca29a
brycx <brycx@protonmail.com>
2019-02-24 12:40:30 +0100

    Doc: Correct misspelling

--- src/hash.rs
+++ src/hash.rs
@@ -40,7 +40,7 @@
 //!
 //! # Security:
 //! - This interface does not support supplying BLAKE2b with a secret key, and
-//!   the hashes retrived
+//!   the hashes retrieved
 //! from using `orion::hash` are therefore not suitable as MACs.
 //! - BLAKE2b is not suitable for password hashing. See `orion::pwhash` instead.
 //!
