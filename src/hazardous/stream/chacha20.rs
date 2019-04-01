6a2d72c6456bdbde2aff6a32256c9591407d8aee
brycx <brycx@protonmail.com>
2019-04-01 22:16:28 +0200

    fmt

--- src/hazardous/stream/chacha20.rs
+++ src/hazardous/stream/chacha20.rs
@@ -140,9 +140,7 @@ struct InternalState {
 }
 
 impl Drop for InternalState {
-	fn drop(&mut self) {
-		self.state.zeroize();
-	}
+	fn drop(&mut self) { self.state.zeroize(); }
 }
 
 impl InternalState {
