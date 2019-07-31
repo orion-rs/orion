4e88f87b17f01fd79a168866b1fbf380ca3dcb85
brycx <brycx@protonmail.com>
2019-07-31 23:45:23 +0200

    0.14.3

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,7 @@
 #![deny(overflowing_literals)]
 #![deny(missing_docs)]
 #![deny(warnings)]
-#![doc(html_root_url = "https://docs.rs/orion/0.14.2")]
+#![doc(html_root_url = "https://docs.rs/orion/0.14.3")]
 
 #[cfg(feature = "safe_api")]
 extern crate getrandom;
