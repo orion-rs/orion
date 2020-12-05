eed2944183842995df6cd7d60db2980dbbdafc37
brycx <brycx@protonmail.com>
2020-12-06 00:02:19 +0100

    Test: Type-based parsing of Wycheproof HKDF tests

--- tests/kdf/wycheproof_hkdf.rs
+++ tests/kdf/wycheproof_hkdf.rs
@@ -1,58 +1,68 @@
 // Testing against Google Wycheproof test vectors
 // Latest commit when these test vectors were pulled: https://github.com/google/wycheproof/commit/2196000605e45d91097147c9c71f26b72af58003
-extern crate hex;
-extern crate serde_json;
 
-use self::hex::decode;
-
-use self::serde_json::{Deserializer, Value};
-use crate::kdf::hkdf_test_runner;
+use hex::decode;
+use serde::{Deserialize, Serialize};
 use std::{fs::File, io::BufReader};
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct WycheproofHkdfTests {
+    algorithm: String,
+    numberOfTests: u64,
+    testGroups: Vec<HkdfTestGroup>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct HkdfTestGroup {
+    keySize: u64,
+    tests: Vec<TestVector>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct TestVector {
+    tcId: u64,
+    comment: String,
+    ikm: String,
+    salt: String,
+    info: String,
+    size: usize,
+    okm: String,
+    result: String,
+    flags: Vec<String>,
+}
 
 fn wycheproof_runner(path: &str) {
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
-    let stream = Deserializer::from_reader(reader).into_iter::<Value>();
+    let tests: WycheproofHkdfTests = serde_json::from_reader(reader).unwrap();
 
-    for test_file in stream {
-        for test_groups in test_file.unwrap().get("testGroups") {
-            for test_group_collection in test_groups.as_array() {
-                for test_group in test_group_collection {
-                    for test_vectors in test_group.get("tests").unwrap().as_array() {
-                        for test_case in test_vectors {
-                            let ikm =
-                                decode(test_case.get("ikm").unwrap().as_str().unwrap()).unwrap();
-                            let salt =
-                                decode(test_case.get("salt").unwrap().as_str().unwrap()).unwrap();
-                            let info =
-                                decode(test_case.get("info").unwrap().as_str().unwrap()).unwrap();
-                            let okm_len = test_case.get("size").unwrap().as_u64().unwrap();
-                            let okm =
-                                decode(test_case.get("okm").unwrap().as_str().unwrap()).unwrap();
-                            let result: bool =
-                                match test_case.get("result").unwrap().as_str().unwrap() {
-                                    "valid" => true,
-                                    "invalid" => false,
-                                    _ => panic!("Unrecognized result detected!"),
-                                };
-                            let tcid = test_case.get("tcId").unwrap().as_u64().unwrap();
-                            println!("tcId: {}, okm_len: {}", tcid, okm_len);
+    let mut tests_run = 0;
+    for test_group in tests.testGroups.iter() {
+        for test in test_group.tests.iter() {
+            let should_test_pass: bool = match test.result.as_str() {
+                "valid" => true,
+                "invalid" => false,
+                _ => panic!("Unexpected test outcome for Wycheproof test"),
+            };
 
-                            hkdf_test_runner(
-                                None,
-                                &okm,
-                                &salt,
-                                &ikm,
-                                &info,
-                                okm_len as usize,
-                                result,
-                            );
-                        }
-                    }
-                }
-            }
+            super::hkdf_test_runner(
+                None,
+                &decode(&test.okm).unwrap(),
+                &decode(&test.salt).unwrap(),
+                &decode(&test.ikm).unwrap(),
+                &decode(&test.info).unwrap(),
+                test.size,
+                should_test_pass,
+            );
+
+            tests_run += 1;
         }
     }
+
+    assert_eq!(tests_run, tests.numberOfTests);
 }
 
 #[test]
