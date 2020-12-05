7e637e85c60561352ee15867b78f73e3fe99c35d
brycx <brycx@protonmail.com>
2020-12-06 00:42:07 +0100

    Test: Type-based parsing of PyNaCl AEAD streaming tests

--- tests/aead/pynacl_streaming_aead.rs
+++ tests/aead/pynacl_streaming_aead.rs
@@ -1,65 +1,65 @@
 // Testing against PyNaCl test vectors
 // Latest commit when these test vectors were pulled: https://github.com/pyca/pynacl/commit/3bb12aef959c92f9042c150deec42cf104c40dfa
 // The generated test vectors have been generated the 26th October 2019.
-extern crate hex;
-extern crate orion;
-extern crate serde_json;
 
-use self::hex::decode;
-use core::convert::TryFrom;
-
-use self::serde_json::{Deserializer, Value};
+use hex::decode;
+use orion::hazardous::aead::streaming::*;
+use serde::{Deserialize, Serialize};
+use std::convert::TryFrom;
 use std::{fs::File, io::BufReader};
 
-use orion::hazardous::aead::streaming::*;
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct TestCase {
+    key: String,
+    header: String,
+    chunks: Vec<TestCaseStreamingMessage>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct TestCaseStreamingMessage {
+    tag: u8,
+    ad: String,
+    message: String,
+    ciphertext: String,
+}
 
 fn run_tests_from_json(path_to_vectors: &str) {
     let file = File::open(path_to_vectors).unwrap();
     let reader = BufReader::new(file);
-    let stream = Deserializer::from_reader(reader).into_iter::<Value>();
+    let tests: Vec<TestCase> = serde_json::from_reader(reader).unwrap();
 
-    for test_file in stream {
-        for test_groups in test_file.unwrap().as_array() {
-            for test_case in test_groups {
-                let key = decode(test_case.get("key").unwrap().as_str().unwrap()).unwrap();
-                let nonce = decode(test_case.get("header").unwrap().as_str().unwrap()).unwrap();
+    for test in tests.iter() {
+        let key = SecretKey::from_slice(&decode(&test.key).unwrap()).unwrap();
+        let nonce = &Nonce::from_slice(&decode(&test.header).unwrap()).unwrap();
 
-                let mut ctx_seal = StreamXChaCha20Poly1305::new(
-                    &SecretKey::from_slice(&key).unwrap(),
-                    &Nonce::from_slice(&nonce).unwrap(),
-                );
+        let mut ctx_seal = StreamXChaCha20Poly1305::new(&key, &nonce);
+        let mut ctx_open = StreamXChaCha20Poly1305::new(&key, &nonce);
 
-                let mut ctx_open = StreamXChaCha20Poly1305::new(
-                    &SecretKey::from_slice(&key).unwrap(),
-                    &Nonce::from_slice(&nonce).unwrap(),
-                );
+        for chunk in test.chunks.iter() {
+            let chunk_msg = decode(&chunk.message).unwrap();
+            let chunk_ct = decode(&chunk.ciphertext).unwrap();
+            let chunk_ad = decode(&chunk.ad).unwrap();
 
-                for stream_chunks in test_case.get("chunks").unwrap().as_array() {
-                    for chunk in stream_chunks {
-                        let chunk_ad = decode(chunk.get("ad").unwrap().as_str().unwrap()).unwrap();
-                        let chunk_tag =
-                            StreamTag::try_from(chunk.get("tag").unwrap().as_u64().unwrap() as u8)
-                                .unwrap();
-                        let chunk_msg =
-                            decode(chunk.get("message").unwrap().as_str().unwrap()).unwrap();
-                        let chunk_ct =
-                            decode(chunk.get("ciphertext").unwrap().as_str().unwrap()).unwrap();
+            let mut chunk_out_ct = vec![0u8; chunk_ct.len()];
+            let mut chunk_out_pt = vec![0u8; chunk_msg.len()];
 
-                        let mut chunk_out_ct = vec![0u8; chunk_ct.len()];
-                        let mut chunk_out_pt = vec![0u8; chunk_msg.len()];
+            ctx_seal
+                .seal_chunk(
+                    &chunk_msg,
+                    Some(&chunk_ad),
+                    &mut chunk_out_ct,
+                    StreamTag::try_from(chunk.tag).unwrap(),
+                )
+                .unwrap();
 
-                        ctx_seal
-                            .seal_chunk(&chunk_msg, Some(&chunk_ad), &mut chunk_out_ct, chunk_tag)
-                            .unwrap();
-                        ctx_open
-                            .open_chunk(&chunk_ct, Some(&chunk_ad), &mut chunk_out_pt)
-                            .unwrap();
+            ctx_open
+                .open_chunk(&chunk_ct, Some(&chunk_ad), &mut chunk_out_pt)
+                .unwrap();
 
-                        assert_eq!(chunk_out_pt, chunk_msg);
-                        assert_eq!(chunk_out_ct, chunk_ct);
-                    }
-                }
-            }
+            assert_eq!(chunk_out_pt, chunk_msg);
+            assert_eq!(chunk_out_ct, chunk_ct);
         }
     }
 }
