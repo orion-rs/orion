c5d41c02cb0df65e63c87c0a647c234177bcfca7
brycx <brycx@protonmail.com>
2021-01-03 21:08:20 +0100

    Update license year and correct files that had a date set prior to their existence in git

--- src/test_framework/mod.rs
+++ src/test_framework/mod.rs
@@ -1,6 +1,6 @@
 // MIT License
 
-// Copyright (c) 2018-2020 The orion Developers
+// Copyright (c) 2019-2021 The orion Developers
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
