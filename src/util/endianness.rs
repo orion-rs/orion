e91eed1acd4855045d5cf04c463021d629bfd5bd
brycx <brycx@protonmail.com>
2021-03-08 19:09:51 +0100

    endianness: Remove now unused u64 BE releated funcs

--- src/util/endianness.rs
+++ src/util/endianness.rs
@@ -75,22 +75,12 @@ impl_load!(u32, u32, from_le_bytes, load_u32_le);
 #[cfg(test)]
 impl_load_into!(u32, u32, from_le_bytes, load_u32_into_le);
 
-// TODO: Test
-impl_load_into!(u32, u32, from_be_bytes, load_u32_into_be);
-
 impl_load_into!(u64, u64, from_le_bytes, load_u64_into_le);
-
-impl_load_into!(u64, u64, from_be_bytes, load_u64_into_be);
 
 impl_store_into!(u32, to_le_bytes, store_u32_into_le);
 
-// TODO: Test
-impl_store_into!(u32, to_be_bytes, store_u32_into_be);
-
 #[cfg(any(feature = "safe_api", feature = "alloc", test))]
 impl_store_into!(u64, to_le_bytes, store_u64_into_le);
-
-impl_store_into!(u64, to_be_bytes, store_u64_into_be);
 
 // Testing public functions in the module.
 #[cfg(test)]
@@ -131,36 +121,30 @@ mod public {
 
     test_empty_src_panic! {test_panic_empty_load_u32_le, &[0u8; 0], [0u32; 4], load_u32_into_le}
     test_empty_src_panic! {test_panic_empty_load_u64_le, &[0u8; 0], [0u64; 4], load_u64_into_le}
-    test_empty_src_panic! {test_panic_empty_load_u64_be, &[0u8; 0], [0u64; 4], load_u64_into_be}
 
     test_empty_src_panic! {test_panic_empty_store_u32_le, &[0u32; 0], [0u8; 24], store_u32_into_le}
     test_empty_src_panic! {test_panic_empty_store_u64_le, &[0u64; 0], [0u8; 24], store_u64_into_le}
-    test_empty_src_panic! {test_panic_empty_store_u64_be, &[0u64; 0], [0u8; 24], store_u64_into_be}
 
     // -1 too low
     test_dst_length_panic! {test_dst_length_load_u32_le_low, &[0u8; 64], [0u32; 15], load_u32_into_le}
     test_dst_length_panic! {test_dst_length_load_u64_le_low, &[0u8; 64], [0u64; 7], load_u64_into_le}
-    test_dst_length_panic! {test_dst_length_load_u64_be_low, &[0u8; 64], [0u64; 7], load_u64_into_be}
 
     test_dst_length_panic! {test_dst_length_store_u32_le_low, &[0u32; 15], [0u8; 64], store_u32_into_le}
     test_dst_length_panic! {test_dst_length_store_u64_le_low, &[0u64; 7], [0u8; 64], store_u64_into_le}
-    test_dst_length_panic! {test_dst_length_store_u64_be_low, &[0u64; 7], [0u8; 64], store_u64_into_be}
+
     // +1 too high
     test_dst_length_panic! {test_dst_length_load_u32_le_high, &[0u8; 64], [0u32; 17], load_u32_into_le}
     test_dst_length_panic! {test_dst_length_load_u64_le_high, &[0u8; 64], [0u64; 9], load_u64_into_le}
-    test_dst_length_panic! {test_dst_length_load_u64_be_high, &[0u8; 64], [0u64; 9], load_u64_into_be}
 
     test_dst_length_panic! {test_dst_length_store_u32_le_high, &[0u32; 17], [0u8; 64], store_u32_into_le}
     test_dst_length_panic! {test_dst_length_store_u64_le_high, &[0u64; 9], [0u8; 64], store_u64_into_le}
-    test_dst_length_panic! {test_dst_length_store_u64_be_high, &[0u64; 9], [0u8; 64], store_u64_into_be}
+
     // Ok
     test_dst_length_ok! {test_dst_length_load_u32_le_ok, &[0u8; 64], [0u32; 16], load_u32_into_le}
     test_dst_length_ok! {test_dst_length_load_u64_le_ok, &[0u8; 64], [0u64; 8], load_u64_into_le}
-    test_dst_length_ok! {test_dst_length_load_u64_be_ok, &[0u8; 64], [0u64; 8], load_u64_into_be}
 
     test_dst_length_ok! {test_dst_length_store_u32_le_ok, &[0u32; 16], [0u8; 64], store_u32_into_le}
     test_dst_length_ok! {test_dst_length_store_u64_le_ok, &[0u64; 8], [0u8; 64], store_u64_into_le}
-    test_dst_length_ok! {test_dst_length_store_u64_be_ok, &[0u64; 8], [0u8; 64], store_u64_into_be}
 
     #[test]
     #[should_panic]
@@ -314,84 +298,6 @@ mod public {
     }
 
     #[test]
-    fn test_results_store_and_load_u64_into_be() {
-        let input_0: [u64; 2] = [588679683042986719, 14213404201893491922];
-        let input_1: [u64; 4] = [
-            11866671478157678302,
-            12365793902795026927,
-            3777757590820648064,
-            6594491344853184185,
-        ];
-        let input_2: [u64; 6] = [
-            2101516190274184922,
-            7904425905466803755,
-            16590119592260157258,
-            6043085125584392657,
-            292831874581513482,
-            1878340435767862001,
-        ];
-        let input_3: [u64; 8] = [
-            10720360125345046831,
-            12576204976780952869,
-            2183760329755932840,
-            12806242450747917237,
-            17861362669514295908,
-            4901620135335484985,
-            3014680565865559727,
-            5106077179490460734,
-        ];
-
-        let expected_0: [u8; 16] = [
-            8, 43, 105, 13, 130, 68, 74, 223, 197, 64, 39, 208, 214, 231, 244, 210,
-        ];
-        let expected_1: [u8; 32] = [
-            164, 174, 226, 214, 73, 217, 22, 222, 171, 156, 32, 9, 173, 201, 241, 239, 52, 109, 74,
-            131, 112, 102, 116, 128, 91, 132, 86, 240, 100, 92, 174, 185,
-        ];
-        let expected_2: [u8; 48] = [
-            29, 42, 21, 215, 59, 6, 102, 218, 109, 178, 41, 123, 72, 190, 134, 43, 230, 59, 241,
-            222, 245, 234, 63, 74, 83, 221, 89, 231, 113, 231, 145, 209, 4, 16, 89, 9, 215, 87,
-            197, 10, 26, 17, 52, 172, 169, 50, 34, 241,
-        ];
-        let expected_3: [u8; 64] = [
-            148, 198, 94, 188, 47, 116, 33, 47, 174, 135, 167, 203, 119, 135, 69, 37, 30, 78, 70,
-            115, 41, 177, 56, 168, 177, 184, 233, 168, 152, 91, 131, 181, 247, 224, 78, 182, 224,
-            210, 138, 100, 68, 6, 13, 139, 14, 146, 222, 57, 41, 214, 76, 0, 143, 176, 182, 175,
-            70, 220, 110, 36, 63, 65, 228, 62,
-        ];
-
-        let mut actual_bytes_0 = [0u8; 16];
-        let mut actual_bytes_1 = [0u8; 32];
-        let mut actual_bytes_2 = [0u8; 48];
-        let mut actual_bytes_3 = [0u8; 64];
-
-        store_u64_into_be(&input_0, &mut actual_bytes_0);
-        store_u64_into_be(&input_1, &mut actual_bytes_1);
-        store_u64_into_be(&input_2, &mut actual_bytes_2);
-        store_u64_into_be(&input_3, &mut actual_bytes_3);
-
-        assert_eq!(actual_bytes_0, expected_0);
-        assert_eq!(actual_bytes_1, expected_1);
-        assert_eq!(actual_bytes_2.as_ref(), expected_2.as_ref());
-        assert_eq!(actual_bytes_3.as_ref(), expected_3.as_ref());
-
-        let mut actual_nums_0 = [0u64; 2];
-        let mut actual_nums_1 = [0u64; 4];
-        let mut actual_nums_2 = [0u64; 6];
-        let mut actual_nums_3 = [0u64; 8];
-
-        load_u64_into_be(&actual_bytes_0, &mut actual_nums_0);
-        load_u64_into_be(&actual_bytes_1, &mut actual_nums_1);
-        load_u64_into_be(&actual_bytes_2, &mut actual_nums_2);
-        load_u64_into_be(&actual_bytes_3, &mut actual_nums_3);
-
-        assert_eq!(actual_nums_0, input_0);
-        assert_eq!(actual_nums_1, input_1);
-        assert_eq!(actual_nums_2, input_2);
-        assert_eq!(actual_nums_3, input_3);
-    }
-
-    #[test]
     fn test_results_load_u32() {
         let input_0: [u8; 4] = [203, 12, 195, 63];
         let expected_0: u32 = 1069747403;
@@ -437,23 +343,6 @@ mod public {
 
     #[quickcheck]
     #[cfg(feature = "safe_api")]
-    /// Load and store should not change the result.
-    fn prop_load_store_u64_be(src: Vec<u8>) -> bool {
-        if !src.is_empty() && src.len() % 8 == 0 {
-            let mut dst_load = vec![0u64; src.len() / 8];
-            load_u64_into_be(&src[..], &mut dst_load);
-            let mut dst_store = src.clone();
-            store_u64_into_be(&dst_load[..], &mut dst_store);
-
-            dst_store == src
-        } else {
-            // Otherwise above functions panic.
-            true
-        }
-    }
-
-    #[quickcheck]
-    #[cfg(feature = "safe_api")]
     /// Store and load should not change the result.
     fn prop_store_load_u32_le(src: Vec<u32>) -> bool {
         let mut dst_store = vec![0u8; src.len() * 4];
@@ -476,18 +365,6 @@ mod public {
         store_u64_into_le(&src[..], &mut dst_store);
         let mut dst_load = src.clone();
         load_u64_into_le(&dst_store[..], &mut dst_load);
-
-        dst_load == src
-    }
-
-    #[quickcheck]
-    #[cfg(feature = "safe_api")]
-    /// Store and load should not change the result.
-    fn prop_store_load_u64_be(src: Vec<u64>) -> bool {
-        let mut dst_store = vec![0u8; src.len() * 8];
-        store_u64_into_be(&src[..], &mut dst_store);
-        let mut dst_load = src.clone();
-        load_u64_into_be(&dst_store[..], &mut dst_load);
 
         dst_load == src
     }
