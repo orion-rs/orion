6148d25bf425fd72d01d32599fa35e86f5d3123a
brycx <brycx@protonmail.com>
2022-08-22 17:29:02 +0200

    sha3: Rho and Pi

--- src/hazardous/hash/sha3/mod.rs
+++ src/hazardous/hash/sha3/mod.rs
@@ -23,6 +23,7 @@
 // TODO:
 // - See if we can make this `const`
 
+/// Round constants. See NIST intermediate test vectors for source.
 const RC: [u64; 24] = [
     0x0000000000000001,
     0x0000000000008082,
@@ -48,6 +49,16 @@ const RC: [u64; 24] = [
     0x8000000000008080,
     0x0000000080000001,
     0x8000000080008008,
+];
+
+/// Rho offsets. See NIST intermediate test vectors for source.
+const RO: [u32; 24] = [
+    1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 2, 14, 27, 41, 56, 8, 25, 43, 62, 18, 39, 61, 20, 44,
+];
+
+/// Indices precomputed based on spec of Pi.
+const PI: [usize; 24] = [
+    10, 7, 11, 17, 18, 3, 5, 16, 8, 21, 24, 4, 15, 23, 19, 13, 12, 2, 20, 14, 22, 9, 6, 1,
 ];
 
 // Theta (θ).
@@ -115,14 +126,116 @@ fn theta(state: &mut [u64; 25]) {
     state[(4 * 5) + 4] ^= buf[(4 + 4) % 5] ^ buf[(4 + 1) % 5].rotate_left(1);
 }
 
-// Rho (ρ).
-fn rho(state: &mut [u64; 25]) {
-    todo!();
-}
+// Rho (ρ) & Pi (π).
+fn rho_and_pi(state: &mut [u64; 25]) {
+    let mut buf = [0u64; 5];
+    let mut prev = state[1];
 
-// Phi (π).
-fn phi(state: &mut [u64; 25]) {
-    todo!();
+    /*
+           unroll24!(x, {
+               array[0] = state[PI[x]];
+               state[PI[x]] = last.rotate_left(RHO[x]);
+               last = array[0];
+           });
+    */
+
+    // NOTE: If these fail test vectors it might require an additional iteration ( the last one )
+
+    buf[0] = state[PI[0]];
+    state[PI[0]] = prev.rotate_left(RO[0]);
+    prev = buf[0];
+
+    buf[0] = state[PI[1]];
+    state[PI[1]] = prev.rotate_left(RO[1]);
+    prev = buf[0];
+
+    buf[0] = state[PI[2]];
+    state[PI[2]] = prev.rotate_left(RO[2]);
+    prev = buf[0];
+
+    buf[0] = state[PI[3]];
+    state[PI[3]] = prev.rotate_left(RO[3]);
+    prev = buf[0];
+
+    buf[0] = state[PI[4]];
+    state[PI[4]] = prev.rotate_left(RO[4]);
+    prev = buf[0];
+
+    buf[0] = state[PI[5]];
+    state[PI[5]] = prev.rotate_left(RO[5]);
+    prev = buf[0];
+
+    buf[0] = state[PI[6]];
+    state[PI[6]] = prev.rotate_left(RO[6]);
+    prev = buf[0];
+
+    buf[0] = state[PI[7]];
+    state[PI[7]] = prev.rotate_left(RO[7]);
+    prev = buf[0];
+
+    buf[0] = state[PI[8]];
+    state[PI[8]] = prev.rotate_left(RO[8]);
+    prev = buf[0];
+
+    buf[0] = state[PI[9]];
+    state[PI[9]] = prev.rotate_left(RO[9]);
+    prev = buf[0];
+
+    buf[0] = state[PI[10]];
+    state[PI[10]] = prev.rotate_left(RO[10]);
+    prev = buf[0];
+
+    buf[0] = state[PI[11]];
+    state[PI[11]] = prev.rotate_left(RO[11]);
+    prev = buf[0];
+
+    buf[0] = state[PI[12]];
+    state[PI[12]] = prev.rotate_left(RO[12]);
+    prev = buf[0];
+
+    buf[0] = state[PI[13]];
+    state[PI[13]] = prev.rotate_left(RO[13]);
+    prev = buf[0];
+
+    buf[0] = state[PI[14]];
+    state[PI[14]] = prev.rotate_left(RO[14]);
+    prev = buf[0];
+
+    buf[0] = state[PI[15]];
+    state[PI[15]] = prev.rotate_left(RO[15]);
+    prev = buf[0];
+
+    buf[0] = state[PI[16]];
+    state[PI[16]] = prev.rotate_left(RO[16]);
+    prev = buf[0];
+
+    buf[0] = state[PI[17]];
+    state[PI[17]] = prev.rotate_left(RO[17]);
+    prev = buf[0];
+
+    buf[0] = state[PI[18]];
+    state[PI[18]] = prev.rotate_left(RO[18]);
+    prev = buf[0];
+
+    buf[0] = state[PI[19]];
+    state[PI[19]] = prev.rotate_left(RO[19]);
+    prev = buf[0];
+
+    buf[0] = state[PI[20]];
+    state[PI[20]] = prev.rotate_left(RO[20]);
+    prev = buf[0];
+
+    buf[0] = state[PI[21]];
+    state[PI[21]] = prev.rotate_left(RO[21]);
+    prev = buf[0];
+
+    buf[0] = state[PI[22]];
+    state[PI[22]] = prev.rotate_left(RO[22]);
+    prev = buf[0];
+
+    buf[0] = state[PI[23]];
+    state[PI[23]] = prev.rotate_left(RO[23]);
+    prev = buf[0];
 }
 
 // Chi (χ).
