921f6a75fe55672f7a6ae14583eea99e651f4f02
brycx <brycx@protonmail.com>
2022-08-22 17:01:01 +0200

    sha3: fmt and round constant

--- src/hazardous/hash/sha3/mod.rs
+++ src/hazardous/hash/sha3/mod.rs
@@ -20,12 +20,35 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-
-
-// TODO: 
+// TODO:
 // - See if we can make this `const`
 
-
+const RC: [u64; 24] = [
+    0x0000000000000001,
+    0x0000000000008082,
+    0x800000000000808A,
+    0x8000000080008000,
+    0x000000000000808B,
+    0x0000000080000001,
+    0x8000000080008081,
+    0x8000000000008009,
+    0x000000000000008A,
+    0x0000000000000088,
+    0x0000000080008009,
+    0x000000008000000A,
+    0x000000008000808B,
+    0x800000000000008B,
+    0x8000000000008089,
+    0x8000000000008003,
+    0x8000000000008002,
+    0x8000000000000080,
+    0x000000000000800A,
+    0x800000008000000A,
+    0x8000000080008081,
+    0x8000000000008080,
+    0x0000000080000001,
+    0x8000000080008008,
+];
 
 // Theta (θ).
 fn theta(state: &mut [u64; 25]) {
@@ -106,8 +129,10 @@ fn phi(state: &mut [u64; 25]) {
 fn chi(state: &mut [u64; 25]) {
     let mut buf = [0u64; 5];
 
-    buf[0] = state[0 + 0]; buf[1] = state[0 + 1];
-    buf[2] = state[0 + 2]; buf[3] = state[0 + 3];
+    buf[0] = state[0 + 0];
+    buf[1] = state[0 + 1];
+    buf[2] = state[0 + 2];
+    buf[3] = state[0 + 3];
     buf[4] = state[0 + 4];
 
     state[0 + 0] = buf[0] ^ ((!buf[(0 + 1) % 5]) & (buf[(0 + 2) % 5]));
@@ -116,8 +141,10 @@ fn chi(state: &mut [u64; 25]) {
     state[0 + 3] = buf[3] ^ ((!buf[(3 + 1) % 5]) & (buf[(3 + 2) % 5]));
     state[0 + 4] = buf[4] ^ ((!buf[(4 + 1) % 5]) & (buf[(4 + 2) % 5]));
 
-    buf[0] = state[5 + 0]; buf[1] = state[5 + 1];
-    buf[2] = state[5 + 2]; buf[3] = state[5 + 3];
+    buf[0] = state[5 + 0];
+    buf[1] = state[5 + 1];
+    buf[2] = state[5 + 2];
+    buf[3] = state[5 + 3];
     buf[4] = state[5 + 4];
 
     state[5 + 0] = buf[0] ^ ((!buf[(0 + 1) % 5]) & (buf[(0 + 2) % 5]));
@@ -126,8 +153,10 @@ fn chi(state: &mut [u64; 25]) {
     state[5 + 3] = buf[3] ^ ((!buf[(3 + 1) % 5]) & (buf[(3 + 2) % 5]));
     state[5 + 4] = buf[4] ^ ((!buf[(4 + 1) % 5]) & (buf[(4 + 2) % 5]));
 
-    buf[0] = state[10 + 0]; buf[1] = state[10 + 1];
-    buf[2] = state[10 + 2]; buf[3] = state[10 + 3];
+    buf[0] = state[10 + 0];
+    buf[1] = state[10 + 1];
+    buf[2] = state[10 + 2];
+    buf[3] = state[10 + 3];
     buf[4] = state[10 + 4];
 
     state[10 + 0] = buf[0] ^ ((!buf[(0 + 1) % 5]) & (buf[(0 + 2) % 5]));
@@ -136,8 +165,10 @@ fn chi(state: &mut [u64; 25]) {
     state[10 + 3] = buf[3] ^ ((!buf[(3 + 1) % 5]) & (buf[(3 + 2) % 5]));
     state[10 + 4] = buf[4] ^ ((!buf[(4 + 1) % 5]) & (buf[(4 + 2) % 5]));
 
-    buf[0] = state[15 + 0]; buf[1] = state[15 + 1];
-    buf[2] = state[15 + 2]; buf[3] = state[15 + 3];
+    buf[0] = state[15 + 0];
+    buf[1] = state[15 + 1];
+    buf[2] = state[15 + 2];
+    buf[3] = state[15 + 3];
     buf[4] = state[15 + 4];
 
     state[15 + 0] = buf[0] ^ ((!buf[(0 + 1) % 5]) & (buf[(0 + 2) % 5]));
@@ -146,8 +177,10 @@ fn chi(state: &mut [u64; 25]) {
     state[15 + 3] = buf[3] ^ ((!buf[(3 + 1) % 5]) & (buf[(3 + 2) % 5]));
     state[15 + 4] = buf[4] ^ ((!buf[(4 + 1) % 5]) & (buf[(4 + 2) % 5]));
 
-    buf[0] = state[20 + 0]; buf[1] = state[20 + 1];
-    buf[2] = state[20 + 2]; buf[3] = state[20 + 3];
+    buf[0] = state[20 + 0];
+    buf[1] = state[20 + 1];
+    buf[2] = state[20 + 2];
+    buf[3] = state[20 + 3];
     buf[4] = state[20 + 4];
 
     state[20 + 0] = buf[0] ^ ((!buf[(0 + 1) % 5]) & (buf[(0 + 2) % 5]));
@@ -161,4 +194,3 @@ fn chi(state: &mut [u64; 25]) {
 fn iota(state: &mut [u64; 25]) {
     todo!();
 }
-
