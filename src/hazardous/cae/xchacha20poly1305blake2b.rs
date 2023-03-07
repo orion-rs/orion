4425b1b002691c63918775acf14cc12e4c1edcf5
Johannes <brycx@protonmail.com>
2023-03-07 12:05:58 +0100

    cargo fmt

--- src/hazardous/cae/xchacha20poly1305blake2b.rs
+++ src/hazardous/cae/xchacha20poly1305blake2b.rs
@@ -20,11 +20,11 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+pub use crate::hazardous::cae::chacha20poly1305blake2b::TAG_SIZE;
+use crate::hazardous::mac::poly1305::POLY1305_OUTSIZE;
 use crate::hazardous::stream::xchacha20::subkey_and_nonce;
 pub use crate::hazardous::stream::{chacha20::SecretKey, xchacha20::Nonce};
 use crate::{errors::UnknownCryptoError, hazardous::aead::chacha20poly1305};
-use crate::hazardous::mac::poly1305::POLY1305_OUTSIZE;
-pub use crate::hazardous::cae::chacha20poly1305blake2b::TAG_SIZE;
 
 #[must_use = "SECURITY WARNING: Ignoring a Result can have real security implications."]
 /// CTX XChaCha20Poly1305 with BLAKE2b-256.
@@ -34,16 +34,17 @@ pub fn seal(
     plaintext: &[u8],
     ad: Option<&[u8]>,
     dst_out: &mut [u8],
-) -> Result<(), UnknownCryptoError> {    
+) -> Result<(), UnknownCryptoError> {
     let (subkey, ietf_nonce) = subkey_and_nonce(secret_key, nonce);
     chacha20poly1305::seal(&subkey, &ietf_nonce, plaintext, ad, dst_out)?;
-    
+
     let mut blake2b = Blake2b::new(32)?;
     blake2b.update(secret_key.unprotected_as_bytes())?;
     blake2b.update(nonce.as_ref())?;
     blake2b.update(ad)?;
     blake2b.update(dst_out[plaintext.len()..plaintext.len() + POLY1305_OUTSIZE])?;
-    dst_out[plaintext.len()..plaintext.len() + TAG_SIZE].copy_from_slice(blake2b.finalize().as_ref());
+    dst_out[plaintext.len()..plaintext.len() + TAG_SIZE]
+        .copy_from_slice(blake2b.finalize().as_ref());
 
     Ok(())
 }
