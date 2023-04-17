6eab5abfb4585f99852f92419ddc6532c480b5c1
brycx <brycx@protonmail.com>
2023-04-17 11:41:30 +0200

    tests: Test also derive_keypair() for Sender

--- tests/hpke/hpke_test_kats.rs
+++ tests/hpke/hpke_test_kats.rs
@@ -13,6 +13,7 @@ pub(crate) struct HpkeTest {
     pub(crate) info: String,
     pub(crate) ikmR: String,
     pub(crate) ikmE: String,
+    pub(crate) ikmS: Option<String>,
     pub(crate) skRm: String,
     pub(crate) skSm: Option<String>,
     pub(crate) skEm: String,
@@ -70,8 +71,12 @@ fn hpke_runner(path: &str) {
                 );
             }
             2 => {
-                let public_sender =
-                    PublicKey::from_slice(&decode(&test.pkSm.unwrap()).unwrap()).unwrap();
+                // We only have values for the sender in this mode
+                let secret_sender = PrivateKey::from_slice(&decode(&test.skSm.unwrap()).unwrap()).unwrap();
+                let public_sender = PublicKey::from_slice(&decode(&test.pkSm.unwrap()).unwrap()).unwrap();
+                let derived_kp = DhKem::derive_keypair(&decode(&test.ikmS.unwrap()).unwrap()).unwrap();
+                assert_eq!(secret_sender, derived_kp.0);
+                assert_eq!(public_sender, derived_kp.1);
 
                 let shared = DhKem::auth_decap(&public_eph, &secret_recip, &public_sender).unwrap();
                 assert_eq!(
