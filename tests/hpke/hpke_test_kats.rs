eba35bb88bb05926d8068252d4a536d913a7bf97
brycx <brycx@protonmail.com>
2025-04-06 11:46:24 +0200

    hpke: Include deterministic sender in KAT tests

--- tests/hpke/hpke_test_kats.rs
+++ tests/hpke/hpke_test_kats.rs
@@ -58,29 +58,67 @@ fn hpke_runner(path: &str) {
             continue;
         }
 
-        let info = hex::decode(test.info).unwrap();
-        let enc = hex::decode(test.enc).unwrap();
-        let shared_secret = hex::decode(test.shared_secret).unwrap();
-        let base_nonce = hex::decode(test.base_nonce).unwrap();
-        let exporter_secret = hex::decode(test.exporter_secret).unwrap();
+        // NOTE: There is a bug in the HPKE test vectors where the private key are not clamped,
+        // so we cannot compare that output: https://github.com/cfrg/draft-irtf-cfrg-hpke/issues/255.
+        // Test that we get the same values for derived keys.
+        if let Some(ikm_s) = test.ikmS {
+            assert!(
+                test.mode as u8 == ModeAuth::<DHKEM_X25519_SHA256_CHACHA20>::MODE_ID
+                    || test.mode as u8 == ModeAuthPsk::<DHKEM_X25519_SHA256_CHACHA20>::MODE_ID
+            );
+
+            let secret_sender =
+                PrivateKey::from_slice(&decode(test.skSm.as_ref().unwrap()).unwrap()).unwrap();
+            let public_sender =
+                PublicKey::from_slice(&decode(test.pkSm.as_ref().unwrap()).unwrap()).unwrap();
+            let derived_kp_sender = DhKem::derive_keypair(&decode(ikm_s).unwrap()).unwrap();
+            assert_eq!(secret_sender, derived_kp_sender.0);
+            assert_eq!(public_sender, derived_kp_sender.1);
+            assert_eq!(
+                &public_sender.to_bytes(),
+                decode(test.pkSm.as_ref().unwrap()).unwrap().as_slice()
+            );
+        }
 
         let secret_recip = PrivateKey::from_slice(&decode(&test.skRm).unwrap()).unwrap();
         let public_recip = PublicKey::from_slice(&decode(&test.pkRm).unwrap()).unwrap();
-        let kem_ciphertext = PublicKey::from_slice(&enc).unwrap();
-        let derived_kp = DhKem::derive_keypair(&decode(&test.ikmR).unwrap()).unwrap();
-        assert_eq!(secret_recip, derived_kp.0);
-        assert_eq!(public_recip, derived_kp.1);
+        let derived_kp_recip = DhKem::derive_keypair(&decode(&test.ikmR).unwrap()).unwrap();
+        assert_eq!(secret_recip, derived_kp_recip.0);
+        assert_eq!(public_recip, derived_kp_recip.1);
+        assert_eq!(
+            &public_recip.to_bytes(),
+            decode(&test.pkRm).unwrap().as_slice()
+        );
 
         let secret_eph = PrivateKey::from_slice(&decode(&test.skEm).unwrap()).unwrap();
         let public_eph = PublicKey::from_slice(&decode(&test.pkEm).unwrap()).unwrap();
-        let derived_kp = DhKem::derive_keypair(&decode(&test.ikmE).unwrap()).unwrap();
-        assert_eq!(secret_eph, derived_kp.0);
-        assert_eq!(public_eph, derived_kp.1);
+        let derived_kp_eph = DhKem::derive_keypair(&decode(&test.ikmE).unwrap()).unwrap();
+        assert_eq!(secret_eph, derived_kp_eph.0);
+        assert_eq!(public_eph, derived_kp_eph.1);
+        assert_eq!(
+            &public_eph.to_bytes(),
+            decode(&test.pkEm).unwrap().as_slice()
+        );
+
+        let info = hex::decode(test.info).unwrap();
+        let shared_secret = hex::decode(test.shared_secret).unwrap();
+        let base_nonce = hex::decode(test.base_nonce).unwrap();
+        let exporter_secret = hex::decode(test.exporter_secret).unwrap();
+        let enc = PublicKey::from_slice(&hex::decode(test.enc).unwrap()).unwrap();
 
         match test.mode as u8 {
             ModeBase::<DHKEM_X25519_SHA256_CHACHA20>::MODE_ID => {
-                let mut hpke_ctx = ModeBase::<DHKEM_X25519_SHA256_CHACHA20>::new_recipient(
-                    &kem_ciphertext,
+                let (mut hpke_ctx_s, actual_enc) =
+                    ModeBase::<DHKEM_X25519_SHA256_CHACHA20>::new_sender_deterministic(
+                        &public_recip,
+                        &info,
+                        secret_eph,
+                    )
+                    .unwrap();
+                assert_eq!(actual_enc, enc);
+
+                let mut hpke_ctx_r = ModeBase::<DHKEM_X25519_SHA256_CHACHA20>::new_recipient(
+                    &enc,
                     &secret_recip,
                     &info,
                 )
@@ -96,8 +134,14 @@ fn hpke_runner(path: &str) {
                     let nonce = hex::decode(&encryption.nonce).unwrap();
                     let pt = hex::decode(&encryption.pt).unwrap();
                     let mut out = vec![0u8; ct.len() - 16];
+                    let mut out_ct = vec![0u8; ct.len()];
 
-                    hpke_ctx
+                    hpke_ctx_s
+                        .seal(&pt, &aad, &mut out_ct)
+                        .expect("Failed encryption");
+                    assert_eq!(&out_ct, &ct);
+
+                    hpke_ctx_r
                         .open(&ct, &aad, &mut out)
                         .expect("Failed decryption");
                     assert_eq!(&pt, &out);
@@ -112,8 +156,19 @@ fn hpke_runner(path: &str) {
                 let psk = hex::decode(test.psk.unwrap()).unwrap();
                 let psk_id = hex::decode(test.psk_id.unwrap()).unwrap();
 
-                let mut hpke_ctx = ModePsk::<DHKEM_X25519_SHA256_CHACHA20>::new_recipient(
-                    &kem_ciphertext,
+                let (mut hpke_ctx_s, actual_enc) =
+                    ModePsk::<DHKEM_X25519_SHA256_CHACHA20>::new_sender_deterministic(
+                        &public_recip,
+                        &info,
+                        &psk,
+                        &psk_id,
+                        secret_eph,
+                    )
+                    .unwrap();
+                assert_eq!(actual_enc, enc);
+
+                let mut hpke_ctx_r = ModePsk::<DHKEM_X25519_SHA256_CHACHA20>::new_recipient(
+                    &enc,
                     &secret_recip,
                     &info,
                     &psk,
@@ -131,8 +186,14 @@ fn hpke_runner(path: &str) {
                     let nonce = hex::decode(&encryption.nonce).unwrap();
                     let pt = hex::decode(&encryption.pt).unwrap();
                     let mut out = vec![0u8; ct.len() - 16];
+                    let mut out_ct = vec![0u8; ct.len()];
 
-                    hpke_ctx
+                    hpke_ctx_s
+                        .seal(&pt, &aad, &mut out_ct)
+                        .expect("Failed encryption");
+                    assert_eq!(&out_ct, &ct);
+
+                    hpke_ctx_r
                         .open(&ct, &aad, &mut out)
                         .expect("Failed decryption");
                     assert_eq!(&pt, &out);
@@ -142,11 +203,24 @@ fn hpke_runner(path: &str) {
             }
             ModeAuth::<DHKEM_X25519_SHA256_CHACHA20>::MODE_ID => {
                 assert!(test.pkSm.is_some());
+                assert!(test.skSm.is_some());
+                let secret_sender =
+                    PrivateKey::from_slice(&decode(test.skSm.unwrap()).unwrap()).unwrap();
                 let public_sender =
                     PublicKey::from_slice(&decode(test.pkSm.unwrap()).unwrap()).unwrap();
 
-                let mut hpke_ctx = ModeAuth::<DHKEM_X25519_SHA256_CHACHA20>::new_recipient(
-                    &kem_ciphertext,
+                let (mut hpke_ctx_s, actual_enc) =
+                    ModeAuth::<DHKEM_X25519_SHA256_CHACHA20>::new_sender_deterministic(
+                        &public_recip,
+                        &info,
+                        &secret_sender,
+                        secret_eph,
+                    )
+                    .unwrap();
+                assert_eq!(actual_enc, enc);
+
+                let mut hpke_ctx_r = ModeAuth::<DHKEM_X25519_SHA256_CHACHA20>::new_recipient(
+                    &enc,
                     &secret_recip,
                     &info,
                     &public_sender,
@@ -163,8 +237,14 @@ fn hpke_runner(path: &str) {
                     let nonce = hex::decode(&encryption.nonce).unwrap();
                     let pt = hex::decode(&encryption.pt).unwrap();
                     let mut out = vec![0u8; ct.len() - 16];
+                    let mut out_ct = vec![0u8; ct.len()];
 
-                    hpke_ctx
+                    hpke_ctx_s
+                        .seal(&pt, &aad, &mut out_ct)
+                        .expect("Failed encryption");
+                    assert_eq!(&out_ct, &ct);
+
+                    hpke_ctx_r
                         .open(&ct, &aad, &mut out)
                         .expect("Failed decryption");
                     assert_eq!(&pt, &out);
@@ -174,16 +254,32 @@ fn hpke_runner(path: &str) {
             }
             ModeAuthPsk::<DHKEM_X25519_SHA256_CHACHA20>::MODE_ID => {
                 assert!(test.pkSm.is_some());
+                assert!(test.skSm.is_some());
                 assert!(test.psk.is_some());
                 assert!(test.psk_id.is_some());
-                let public_sender =
-                    PublicKey::from_slice(&decode(test.pkSm.unwrap()).unwrap()).unwrap();
 
                 let psk = hex::decode(test.psk.unwrap()).unwrap();
                 let psk_id = hex::decode(test.psk_id.unwrap()).unwrap();
 
-                let mut hpke_ctx = ModeAuthPsk::<DHKEM_X25519_SHA256_CHACHA20>::new_recipient(
-                    &kem_ciphertext,
+                let secret_sender =
+                    PrivateKey::from_slice(&decode(test.skSm.unwrap()).unwrap()).unwrap();
+                let public_sender =
+                    PublicKey::from_slice(&decode(test.pkSm.unwrap()).unwrap()).unwrap();
+
+                let (mut hpke_ctx_s, actual_enc) =
+                    ModeAuthPsk::<DHKEM_X25519_SHA256_CHACHA20>::new_sender_deterministic(
+                        &public_recip,
+                        &info,
+                        &psk,
+                        &psk_id,
+                        &secret_sender,
+                        secret_eph,
+                    )
+                    .unwrap();
+                assert_eq!(actual_enc, enc);
+
+                let mut hpke_ctx_r = ModeAuthPsk::<DHKEM_X25519_SHA256_CHACHA20>::new_recipient(
+                    &enc,
                     &secret_recip,
                     &info,
                     &psk,
@@ -202,8 +298,14 @@ fn hpke_runner(path: &str) {
                     let nonce = hex::decode(&encryption.nonce).unwrap();
                     let pt = hex::decode(&encryption.pt).unwrap();
                     let mut out = vec![0u8; ct.len() - 16];
+                    let mut out_ct = vec![0u8; ct.len()];
 
-                    hpke_ctx
+                    hpke_ctx_s
+                        .seal(&pt, &aad, &mut out_ct)
+                        .expect("Failed encryption");
+                    assert_eq!(&out_ct, &ct);
+
+                    hpke_ctx_r
                         .open(&ct, &aad, &mut out)
                         .expect("Failed decryption");
                     assert_eq!(&pt, &out);
