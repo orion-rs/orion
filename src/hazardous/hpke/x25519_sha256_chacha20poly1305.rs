0c38dabacb6437b13ba8f23ba4a9027c1d7b6682
Johannes <brycx@protonmail.com>
2026-07-24 10:25:13 +0200

    clippy: new linting rules (#644)

--- src/hazardous/hpke/x25519_sha256_chacha20poly1305.rs
+++ src/hazardous/hpke/x25519_sha256_chacha20poly1305.rs
@@ -69,7 +69,7 @@ impl Eq for DHKEM_X25519_SHA256_CHACHA20 {}
 impl core::fmt::Debug for DHKEM_X25519_SHA256_CHACHA20 {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(f, "{} key: {{***OMITTED***}}, base_nonce: {:?}, ctr: {:?}, exporter_secret: {{***OMITTED***}}",
-            stringify!(DHKEM_X25519_SHA256_CHACHA20), &self.base_nonce, self.ctr)
+            stringify!(DHKEM_X25519_SHA256_CHACHA20), self.base_nonce, self.ctr)
     }
 }
 
@@ -552,10 +552,10 @@ mod test {
         let (_sk, pk) = DhKem::derive_keypair(&[0u8; 64]).unwrap();
         let (ctx, _enc) = DHKEM_X25519_SHA256_CHACHA20::setup_base_sender(&pk, &[0u8; 64]).unwrap();
 
-        let secret_key = format!("{:?}", &ctx.key);
-        let secret_export = format!("{:?}", &ctx.exporter_secret);
+        let secret_key = format!("{:?}", ctx.key);
+        let secret_export = format!("{:?}", ctx.exporter_secret);
 
-        let test_debug_contents = format!("{:?}", &ctx);
+        let test_debug_contents = format!("{:?}", ctx);
         assert!(!test_debug_contents.contains(&secret_key));
         assert!(!test_debug_contents.contains(&secret_export));
     }
