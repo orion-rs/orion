e7147886c163aa591bdfbfbe4c268b32404fdedf
brycx <brycx@protonmail.com>
2026-07-24 10:10:16 +0200

    clippy: new linting rules

--- src/hazardous/ecc/x25519.rs
+++ src/hazardous/ecc/x25519.rs
@@ -106,7 +106,7 @@ impl PartialEq for FieldElement {
 
 impl core::fmt::Debug for FieldElement {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "FieldElement({:?})", &self.0 .0)
+        write!(f, "FieldElement({:?})", self.0 .0)
     }
 }
 
