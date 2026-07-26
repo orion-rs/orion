57d62cd381ed4712cf5de6c427eba0517200ba3c
brycx <brycx@protonmail.com>
2026-07-26 11:04:05 +0200

    hpke: Fix safe_api gate

--- src/hazardous/hpke/kem.rs
+++ src/hazardous/hpke/kem.rs
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#[cfg(feature = "safe_api")]
 use crate::Secret;
 use crate::{
     errors::UnknownCryptoError,
