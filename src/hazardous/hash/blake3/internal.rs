83a997da39756a2916446d281b0d31abb98c3b2a
Jakub Mata <matajakub@email.cz>
2026-07-26 17:47:36 +0200

    Add licence

--- src/hazardous/hash/blake3/internal.rs
+++ src/hazardous/hash/blake3/internal.rs
@@ -1,3 +1,25 @@
+// MIT License
+
+// Copyright (c) 2026 The orion Developers
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 use core::array;
 use core::cmp::min;
 use core::ops::{Index, IndexMut};
@@ -86,7 +108,7 @@ impl ChunkState {
     /// Consumes the provided `input`. The size of the input must not be
     /// greater than CHUNK_LEN - len().
     pub(crate) fn update(&mut self, input: &[u8]) {
-        assert!(input.len() <= CHUNK_LEN - self.len());
+        debug_assert!(input.len() <= CHUNK_LEN - self.len());
         let mut input_view = input;
 
         while !input_view.is_empty() {
